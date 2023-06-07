7050679747e71c7b1ffc50168908e94c185f1475
heart <heart@dzpk.onmicrosoft.com>
2023-06-07 08:06:11 +0800

    添加注单枚举，rpc

---
+++ src/main/java/com/dx/facade/enums/PokerCodeEnum.java
@@ -0,0 +1,115 @@
+package com.dx.facade.enums;
+
+/**
+ * 扑克牌
+ * @author heart
+ */
+public enum PokerCodeEnum {
+    KING1("KING_1", "大王" ,"red peach a", "紅桃a", "ハートa"),
+    KING2("KONG_2", "小王" ,"block A", "紅桃a", "ブロックA"),
+
+    A1("A_1", "红桃A" ,"red peach a", "紅桃a", "ハートa"),
+    A2("A_2", "方块A" ,"block A", "紅桃a", "ブロックA"),
+    A3("A_3", "梅花A" ,"plum blossom A", "梅花A", "梅の花"),
+    A4("A_4", "黑体A" ,"blackbody A", "黑體A", "黒体A"),
+
+    ONE1("ONE_1", "红桃1" ,"red peach a", "紅桃a", "ハートa"),
+    ONE2("ONE_2", "方块1" ,"block A", "紅桃a", "ブロックA"),
+    ONE3("ONE_3", "梅花1" ,"plum blossom A", "梅花A", "梅の花"),
+    ONE4("ONE_4", "黑体1" ,"blackbody A", "黑體A", "黒体A"),
+
+    TWO1("TWO_1", "红桃2" ,"red peach a", "紅桃a", "ハートa"),
+    TWO2("TWO_2", "方块2" ,"block A", "紅桃a", "ブロックA"),
+    TWO3("TWO_3", "梅花2" ,"plum blossom A", "梅花A", "梅の花"),
+    TWO4("TWO_4", "黑体2" ,"blackbody A", "黑體A", "黒体A"),
+
+    THREE1("THREE_1", "红桃3" ,"red peach a", "紅桃a", "ハートa"),
+    THREE2("THREE_2", "方块3" ,"block A", "紅桃a", "ブロックA"),
+    THREE3("THREE_3", "梅花3" ,"plum blossom A", "梅花A", "梅の花"),
+    THREE4("THREE_4", "黑体3" ,"blackbody A", "黑體A", "黒体A"),
+
+    FOUR1("FOUR_1", "红桃4" ,"red peach a", "紅桃a", "ハートa"),
+    FOUR2("FOUR_2", "方块4" ,"block A", "紅桃a", "ブロックA"),
+    FOUR3("FOUR_3", "梅花4" ,"plum blossom A", "梅花A", "梅の花"),
+    FOUR4("FOUR_4", "黑体4" ,"blackbody A", "黑體A", "黒体A"),
+
+    FIVE1("FIVE_1", "红桃5" ,"red peach a", "紅桃a", "ハートa"),
+    FIVE2("FIVE_2", "方块5" ,"block A", "紅桃a", "ブロックA"),
+    FIVE3("FIVE_3", "梅花5" ,"plum blossom A", "梅花A", "梅の花"),
+    FIVE4("FIVE_4", "黑体5" ,"blackbody A", "黑體A", "黒体A"),
+
+    SIX1("SIX_1", "红桃6" ,"red peach a", "紅桃a", "ハートa"),
+    SIX2("SIX_2", "方块6" ,"block A", "紅桃a", "ブロックA"),
+    SIX3("SIX_3", "梅花6" ,"plum blossom A", "梅花A", "梅の花"),
+    SIX4("SIX_4", "黑体6" ,"blackbody A", "黑體A", "黒体A"),
+
+    SEVEN1("SEVEN_1", "红桃7" ,"red peach a", "紅桃a", "ハートa"),
+    SEVEN2("SEVEN_2", "方块7" ,"block A", "紅桃a", "ブロックA"),
+    SEVEN3("SEVEN_3", "梅花7" ,"plum blossom A", "梅花A", "梅の花"),
+    SEVEN4("SEVEN_4", "黑体7" ,"blackbody A", "黑體A", "黒体A"),
+
+    EIGHT1("EIGHT_1", "红桃8" ,"red peach a", "紅桃a", "ハートa"),
+    EIGHT2("EIGHT_2", "方块8" ,"block A", "紅桃a", "ブロックA"),
+    EIGHT3("EIGHT_3", "梅花8" ,"plum blossom A", "梅花A", "梅の花"),
+    EIGHT4("EIGHT_4", "黑体8" ,"blackbody A", "黑體A", "黒体A"),
+
+    NINE1("NINE_1", "红桃9" ,"red peach a", "紅桃a", "ハートa"),
+    NINE2("NINE_2", "方块9" ,"block A", "紅桃a", "ブロックA"),
+    NINE3("NINE_3", "梅花9" ,"plum blossom A", "梅花A", "梅の花"),
+    NINE4("NINE_4", "黑体9" ,"blackbody A", "黑體A", "黒体A"),
+
+    TEN1("TEN_1", "红桃10" ,"red peach a", "紅桃a", "ハートa"),
+    TEN2("TEN_2", "方块10" ,"block A", "紅桃a", "ブロックA"),
+    TEN3("TEN_3", "梅花10" ,"plum blossom A", "梅花A", "梅の花"),
+    TEN4("TEN_4", "黑体10" ,"blackbody A", "黑體A", "黒体A"),
+
+    J1("J_1", "红桃J" ,"red peach a", "紅桃a", "ハートa"),
+    J2("J_2", "方块J" ,"block A", "紅桃a", "ブロックA"),
+    J3("J_3", "梅花J" ,"plum blossom A", "梅花A", "梅の花"),
+    J4("J_4", "黑体J" ,"blackbody A", "黑體A", "黒体A"),
+
+    Q1("Q_1", "红桃Q" ,"red peach a", "紅桃a", "ハートa"),
+    Q2("Q_2", "方块Q" ,"block A", "紅桃a", "ブロックA"),
+    Q3("Q_3", "梅花Q" ,"plum blossom A", "梅花A", "梅の花"),
+    Q4("Q_4", "黑体Q" ,"blackbody A", "黑體A", "黒体A"),
+
+    K1("K_1", "红桃K" ,"red peach a", "紅桃a", "ハートa"),
+    K2("K_2", "方块K" ,"block A", "紅桃a", "ブロックA"),
+    K3("K_3", "梅花K" ,"plum blossom A", "梅花A", "梅の花"),
+    K4("K_4", "黑体K" ,"blackbody A", "黑體A", "黒体A");
+    private String code;
+    private String zh;
+    private String en;
+    private String tc;
+    private String ja;
+
+    PokerCodeEnum(String code, String zh, String en, String tc, String ja) {
+        this.code = code;
+        this.zh = zh;
+        this.en = en;
+        this.tc = tc;
+        this.ja = ja;
+    }
+
+
+    public String getCode() {
+        return code;
+    }
+
+    public String getZh() {
+        return zh;
+    }
+
+    public String getEn() {
+        return en;
+    }
+
+    public String getTc() {
+        return tc;
+    }
+
+    public String getJa() {
+        return ja;
+    }
+
+}
