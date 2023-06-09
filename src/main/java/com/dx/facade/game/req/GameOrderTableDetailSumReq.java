096f5ff60d26015a66eff1a219f304a31c9a8bc6
heart <heart@dzpk.onmicrosoft.com>
2023-06-09 19:13:33 +0800

    mq Dto移除

--- src/main/java/com/dx/facade/game/req/GameOrderTableDetailSumReq.java
+++ src/main/java/com/dx/facade/game/req/GameOrderTableDetailSumReq.java
@@ -1,5 +1,6 @@
 package com.dx.facade.game.req;
 
+import com.dx.facade.report.param.base.BaseRequest;
 import com.dx.facade.report.param.base.LocalDatePageRequest;
 import lombok.Data;
 
@@ -7,7 +8,7 @@ import lombok.Data;
  * 牌局详情请求对象
  */
 @Data
-public class GameOrderTableDetailSumReq extends LocalDatePageRequest {
+public class GameOrderTableDetailSumReq extends BaseRequest {
     /**
      * 牌桌id
      */
