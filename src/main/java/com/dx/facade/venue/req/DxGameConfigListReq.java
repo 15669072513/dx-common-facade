1a5b9a11b419a4ec751373ff7b1712efc3876a37
broadway <broadway@dzpk.onmicrosoft.com>
2023-06-16 20:42:54 +0800

    rpc 接口优化

--- src/main/java/com/dx/facade/venue/req/DxGameConfigListReq.java
+++ src/main/java/com/dx/facade/venue/req/DxGameConfigListReq.java
@@ -40,13 +40,13 @@ public class DxGameConfigListReq extends PageRequest implements Serializable {
     private String gameZhName;
 
     @ApiModelProperty("游戏英文名")
-    private Long gameEnName;
+    private String gameEnName;
 
     @ApiModelProperty("游戏泰语名")
     private String gameThName;
 
     @ApiModelProperty("游戏越南名")
-    private Long gameVnName;
+    private String gameVnName;
 
     @ApiModelProperty("支持终端")
     private String supportTerminals;
