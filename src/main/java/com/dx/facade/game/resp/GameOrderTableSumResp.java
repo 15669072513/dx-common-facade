175e708625c85a81dc052a0dd368fb1c7c046084
heart <heart@dzpk.onmicrosoft.com>
2023-06-20 18:45:11 +0800

    手牌列表接口 手牌详情接口修改

--- src/main/java/com/dx/facade/game/resp/GameOrderTableSumResp.java
+++ src/main/java/com/dx/facade/game/resp/GameOrderTableSumResp.java
@@ -9,10 +9,16 @@ import java.math.BigDecimal;
 public class GameOrderTableSumResp {
     @ApiModelProperty("牌桌id")
     private Long tableId;
-    @ApiModelProperty("参数手牌数")
+    @ApiModelProperty("参与手牌数")
     private Long handCount;
     @ApiModelProperty("盈亏金额")
     private BigDecimal netAmount;
     @ApiModelProperty("有效金额")
     private BigDecimal validBetAmount;
+    @ApiModelProperty("首次带入")
+    private BigDecimal firstBringChip;
+    @ApiModelProperty("中途带入")
+    private BigDecimal bringMidwayChip;
+    @ApiModelProperty("总带出")
+    private BigDecimal afterChip;
 }
