51d907e79306a5022a5f1275040b5893754bfeca
broadway <broadway@dzpk.onmicrosoft.com>
2023-06-20 16:42:30 +0800

    rpc 接口优化

---
+++ src/main/java/com/dx/facade/venue/resp/DxTableBaseInfoResp.java
@@ -0,0 +1,82 @@
+package com.dx.facade.venue.resp;
+
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+/**
+ * <p>
+ *
+ * </p>
+ *
+ * @author dereck
+ * @since 2022-10-13
+ */
+@Data
+public class DxTableBaseInfoResp implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @ApiModelProperty("牌桌id")
+    private Long tableId;
+
+    @ApiModelProperty("牌桌code")
+    private String tableCode;
+
+    @ApiModelProperty("牌桌名称")
+    private String tableName;
+
+    @ApiModelProperty("关联游戏id")
+    private Long gameTypeId;
+
+    @ApiModelProperty("关联游戏名称")
+    private String gameName;
+
+    @ApiModelProperty("关联俱乐部id")
+    private Long clubId;
+
+    @ApiModelProperty("俱乐部名称")
+    private String clubName;
+
+    @ApiModelProperty("牌桌状态：0 未开始，1 进行中 2已结束")
+    private Integer status;
+
+    @ApiModelProperty("累计参与者")
+    private Integer participantTotal;
+
+    @ApiModelProperty("牌桌开始时间")
+    private LocalDateTime tableBeginTime;
+
+    @ApiModelProperty("牌桌结束时间")
+    private LocalDateTime tableEndTime;
+
+    @ApiModelProperty("牌桌结束类型：0强制解散 1房间内解散 2正常结束")
+    private Integer overType;
+
+    @ApiModelProperty("手牌数")
+    private Long handCount;
+
+    @ApiModelProperty("盈亏金额")
+    private BigDecimal netAmount;
+
+    @ApiModelProperty("有效金额")
+    private BigDecimal validBetAmount;
+
+    @ApiModelProperty("创建时间")
+    private LocalDateTime createdAt;
+
+    @ApiModelProperty("创建人")
+    private String createdBy;
+
+    @ApiModelProperty("修改时间")
+    private LocalDateTime updatedAt;
+
+    @ApiModelProperty("修改人")
+    private String updatedBy;
+
+    @ApiModelProperty("是否删除（0 未删除 1已删除）")
+    private Integer deleteFlag;
+}
