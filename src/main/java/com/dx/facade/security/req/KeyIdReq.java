27ff6c89923c3a6a31e8151ca7529f645955f623
wuyanzu01 <2524173458@qq.com>
2023-06-21 20:12:25 +0800

    更改安全后台rpc请求参数

--- src/main/java/com/dx/facade/security/req/KeyIdReq.java
+++ src/main/java/com/dx/facade/security/req/KeyIdReq.java
@@ -3,11 +3,15 @@ package com.dx.facade.security.req;
 import com.dx.facade.member.exception.ErrorCode;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.util.Assert;
 
 @ApiModel("通过keyId获取key请求实体")
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class KeyIdReq {
   @ApiModelProperty(value = "keyId", required = true)
   private String keyId;
