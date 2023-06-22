776342f1daebfdfb1bb7cff10113fa9fc3b72685
Seven <seven@dzpk.onmicrosoft.com>
2023-06-22 15:23:40 +0800

    fix(牌桌): 详情缺少文档参数编写

--- src/main/java/com/dx/facade/venue/resp/TableConfigRuleResp.java
+++ src/main/java/com/dx/facade/venue/resp/TableConfigRuleResp.java
@@ -1,81 +1,97 @@
 package com.dx.facade.venue.resp;
 
 import io.swagger.annotations.ApiModelProperty;
+import io.swagger.annotations.ApiOperation;
 import lombok.Data;
 
 import java.math.BigDecimal;
 
 @Data
+@ApiOperation("牌桌详情")
 public class TableConfigRuleResp {
 
     /***
      * 房主id
      */
+    @ApiModelProperty("房主id")
     private Long holderId;
 
     /***
      * 牌桌id
      */
+    @ApiModelProperty("牌桌id")
     private Long tableId;
 
     /***
      * 牌桌code
      */
+    @ApiModelProperty("牌桌code")
     private String tableCode;
 
     /***
      * 牌桌类型 2001L:德州局 2002L:短牌局
      */
+    @ApiModelProperty("牌桌类型 2001L:德州局 2002L:短牌局")
     private Long tableType;
 
     /***
      * 牌桌名称
      */
+    @ApiModelProperty("牌桌名称")
     private String tableName;
 
     /***
      * 牌桌最大人数
      */
+    @ApiModelProperty("牌桌最大人数")
     private Integer maxSeats;
 
     /***
      * 自动开局人数
      */
+    @ApiModelProperty("自动开局人数")
     private Integer autoStartUserCounts;
 
     /**
      * 默认带入的筹码
      */
+    @ApiModelProperty("默认带入的筹码")
     private BigDecimal bringChipScore;
 
     /***
      * 小盲注
      */
+    @ApiModelProperty("小盲注")
     private BigDecimal sbBlindScore;
 
     /***
      * 大盲注
      */
+    @ApiModelProperty("大盲注")
     private BigDecimal bbBlindScore;
 
     /**
      * 时长 单位 分钟
      */
+    @ApiModelProperty("时长 单位 分钟")
     private Integer timeCount;
 
     /**
      * 最小带入的筹码
      */
+    @ApiModelProperty("最小带入的筹码")
     private BigDecimal minBringChipScore;
 
     /**
      * 最大带入的筹码
      */
+    @ApiModelProperty("最大带入的筹码")
     private BigDecimal maxBringChipScore;
 
     /**
      * 抽水比例配置
      */
+    @ApiModelProperty("抽水比例配置")
     private BigDecimal rake;
 
     /** 人员上线 */
