21d88a773a3b935eef7bc7fe3a5f487c4a1cf8e8
wuyanzu01 <2524173458@qq.com>
2023-06-22 15:05:20 +0800

    添加安全后台rpc接口，获取多个key

--- src/main/java/com/dx/facade/security/service/SecurityKeyRPCService.java
+++ src/main/java/com/dx/facade/security/service/SecurityKeyRPCService.java
@@ -3,12 +3,16 @@ package com.dx.facade.security.service;
 import com.dx.entity.CommonResp;
 import com.dx.facade.security.dto.SecurityKeyInfoDTO;
 import com.dx.facade.security.req.KeyIdReq;
+import com.dx.facade.security.req.ListKeyReq;
 import com.dx.facade.security.req.SecurityKeyReq;
+
+import java.util.List;
 
 public interface SecurityKeyRPCService {
 
   /**
    * 根据platformId、applicationId、version获取key
+   *
    * @param req
    * @return
    */
@@ -16,8 +20,17 @@ public interface SecurityKeyRPCService {
 
   /**
    * 根据keyId获取key
+   *
    * @param req
    * @return
    */
   CommonResp<SecurityKeyInfoDTO> getKeyByKeyId(KeyIdReq req) throws Exception;
+
+  /**
+   * 根据多个keyId获取多个key
+   * @param req
+   * @return
+   * @throws Exception
+   */
+  CommonResp<List<SecurityKeyInfoDTO>> getListKey(ListKeyReq req) throws Exception;
 }
