c68366972bca9c7311d5ae94f4aab97416e37a73
bicycle <bicycle@dzpk.onmicrosoft.com>
2023-06-22 15:33:31 +0800

    feat:添加用户类型

--- src/main/java/com/dx/facade/member/rep/MemberRemarkParam.java
+++ src/main/java/com/dx/facade/member/rep/MemberRemarkParam.java
@@ -20,13 +20,15 @@ public class MemberRemarkParam implements Serializable {
 
     @ApiModelProperty(value = "账号(20)",required = true,example = "abc132")
     @NotBlank(message = "账号不能为空")
-    //@Pattern(regexp = BWManagerConstant.REG_4_11, message = "账号 4-11位，最少1个字母+数字组合，首位字母")
     private String userName;
 
     @ApiModelProperty(value = "用户id(20)",required = true,example = "2817562767709381444")
     @NotBlank(message = "用户ID不能为空")
-    //@Pattern(regexp = BWManagerConstant.REG_NUM_6_20, message = "会员Id:请输入6-20位数字")
     private String userId;
+    
+    /** 用户类型 0：会员 1：代理*/
+    @ApiModelProperty(value = "用户类型 0：会员 1：代理",required = true,example = "1")
+    private Integer userType;
 
     @ApiModelProperty(value = "备注说明",required = true,example = "备注...")
     @NotBlank(message = "备注说明不能为空")
