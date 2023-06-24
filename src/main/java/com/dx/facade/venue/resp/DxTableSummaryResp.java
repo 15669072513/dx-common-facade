3632623f4914638d807c1773a72a18d7cc44d9c3
broadway <broadway@dzpk.onmicrosoft.com>
2023-06-24 14:15:14 +0800

    rpc 接口优化

--- src/main/java/com/dx/facade/venue/resp/DxTableSummaryResp.java
+++ src/main/java/com/dx/facade/venue/resp/DxTableSummaryResp.java
@@ -23,8 +23,8 @@ public class DxTableSummaryResp implements Serializable {
     private static final long serialVersionUID = 1L;
 
     @ApiModelProperty("总牌桌抽水")
-    private BigDecimal pumpAmountSum;
+    private BigDecimal pumpAmountSum = BigDecimal.ZERO;
 
     @ApiModelProperty("总牌桌费用")
-    private BigDecimal costSum;
+    private BigDecimal costSum = BigDecimal.ZERO;
 }
