fdf3cea3adbcf19cabc0bf5acde664e4d2cbba70
two <>
2023-06-27 19:46:34 +0800

    add proxy summary detail

--- src/main/java/com/dx/facade/report/resp/proxynetamountday/ProxyNetAmountSummaryDayBackRespDTO.java
+++ src/main/java/com/dx/facade/report/resp/proxynetamountday/ProxyNetAmountSummaryDayBackRespDTO.java
@@ -1,5 +1,6 @@
 package com.dx.facade.report.resp.proxynetamountday;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -22,172 +23,80 @@ import java.math.BigDecimal;
 @AllArgsConstructor
 public class ProxyNetAmountSummaryDayBackRespDTO {
 
-    /**
-     * betCountTotal
-     */
-    @ApiModelProperty(value = "注单量", example = "200")
-    private Long betCount = 0L;
+    /** betAmount */
+    @ApiModelProperty(value = "总投注", example = "1500.00")
+    private BigDecimal betAmount;
 
-    /**
-     * memberCountTotal
-     */
+    /** validBetAmount */
+    @ApiModelProperty(value = "总有效投注", example = "1500.00")
+    private BigDecimal validBetAmount;
+
+    /** flowCorrection */
+    @ApiModelProperty(value = "流水纠正", example = "1500.00")
+    private BigDecimal flowCorrection;
+
+    /** memberCount */
     @ApiModelProperty(value = "投注人数", example = "100")
-    private Long memberCount = 0L;
+    private Long memberCount;
 
-    /**
-     * betAmountTotal
-     */
-    @ApiModelProperty(value = "总投注CNY", example = "1500.00")
-    private BigDecimal betAmountCNY = BigDecimal.ZERO;
-    @ApiModelProperty(value = "总投注VND", example = "1500.00")
-    private BigDecimal betAmountVND = BigDecimal.ZERO;
-    @ApiModelProperty(value = "总投注THB", example = "1500.00")
-    private BigDecimal betAmountTHB = BigDecimal.ZERO;
+    /** betCount */
+    @ApiModelProperty(value = "注单量", example = "200")
+    private Long betCount;
 
-    /**
-     * validBetAmountTotal
-     */
-    @ApiModelProperty(value = "总有效投注CNY", example = "1500.00")
-    private BigDecimal validBetAmountCNY = BigDecimal.ZERO;
-    @ApiModelProperty(value = "总有效投注VND", example = "1500.00")
-    private BigDecimal validBetAmountVND = BigDecimal.ZERO;
-    @ApiModelProperty(value = "总有效投注合计THB", example = "1500.00")
-    private BigDecimal validBetAmountTHB = BigDecimal.ZERO;
+    /** netAmount */
+    @ApiModelProperty(value = "会员总输赢", example = "1500.00")
+    private BigDecimal netAmount;
 
-    /**
-     * netAmountTotal
-     */
-    @ApiModelProperty(value = "投注盈亏CNY", example = "1500.00")
-    private BigDecimal netAmountCNY = BigDecimal.ZERO;
-    @ApiModelProperty(value = "投注盈亏VND", example = "1500.00")
-    private BigDecimal netAmountVND = BigDecimal.ZERO;
-    @ApiModelProperty(value = "投注盈亏THB", example = "1500.00")
-    private BigDecimal netAmountTHB = BigDecimal.ZERO;
+    /** venueFee */
+    @ApiModelProperty(value = "场馆费", example = "1500.00")
+    private BigDecimal venueFee;
 
-    /**
-     * venueFeeTotal
-     */
-    @ApiModelProperty(value = "场馆费CNY", example = "1500.00")
-    private BigDecimal venueFeeCNY = BigDecimal.ZERO;
-    @ApiModelProperty(value = "场馆费VND", example = "1500.00")
-    private BigDecimal venueFeeVND = BigDecimal.ZERO;
-    @ApiModelProperty(value = "场馆费THB", example = "1500.00")
-    private BigDecimal venueFeeTHB = BigDecimal.ZERO;
+    /** rebateAmount */
+    @ApiModelProperty(value = "会员返水", example = "1500.00")
+    private BigDecimal rebateAmount;
 
-    /**
-     * rebateAmountTotal
-     */
-    @ApiModelProperty(value = "总返水CNY", example = "1500.00")
-    private BigDecimal rebateAmountCNY = BigDecimal.ZERO;
-    @ApiModelProperty(value = "总返水VND", example = "1500.00")
-    private BigDecimal rebateAmountVND = BigDecimal.ZERO;
-    @ApiModelProperty(value = "总返水THB", example = "1500.00")
-    private BigDecimal rebateAmountTHB = BigDecimal.ZERO;
+    /** discountAmount */
+    @ApiModelProperty(value = "会员优惠", example = "1500.00")
+    private BigDecimal discountAmount;
 
-    /**
-     * discountAmountTotal
-     */
-    @ApiModelProperty(value = "总优惠CNY", example = "1500.00")
-    private BigDecimal discountAmountCNY = BigDecimal.ZERO;
-    @ApiModelProperty(value = "总优惠VND", example = "1500.00")
-    private BigDecimal discountAmountVND = BigDecimal.ZERO;
-    @ApiModelProperty(value = "总优惠THB", example = "1500.00")
-    private BigDecimal discountAmountTHB = BigDecimal.ZERO;
+    /** proxyRebatePoint */
+    @ApiModelProperty(value = "代理返点", example = "1500.00")
+    private BigDecimal proxyRebatePoint;
 
-    /**
-     * proxyRebatePointTotal
-     */
-    @ApiModelProperty(value = "总返点CNY", example = "1500.00")
-    private BigDecimal proxyRebatePointCNY = BigDecimal.ZERO;
-    @ApiModelProperty(value = "总返点VND", example = "1500.00")
-    private BigDecimal proxyRebatePointVND = BigDecimal.ZERO;
-    @ApiModelProperty(value = "总返点THB", example = "1500.00")
-    private BigDecimal proxyRebatePointTHB = BigDecimal.ZERO;
+    /** artificialPatchAmount */
+    @ApiModelProperty(value = "会员账户调整", example = "1500.00")
+    private BigDecimal artificialPatchAmount;
 
-    /**
-     * artificialPatchAmountTotal
-     */
-    @ApiModelProperty(value = "其他调整CNY", example = "1500.00")
-    private BigDecimal artificialPatchAmountCNY = BigDecimal.ZERO;
-    @ApiModelProperty(value = "其他调整VND", example = "1500.00")
-    private BigDecimal artificialPatchAmountVND = BigDecimal.ZERO;
-    @ApiModelProperty(value = "其他调整THB", example = "1500.00")
-    private BigDecimal artificialPatchAmountTHB = BigDecimal.ZERO;
+    /** repairBetAmount */
+    @ApiModelProperty(value = "补单输赢", example = "1500.00")
+    private BigDecimal repairBetAmount;
 
-    /**
-     * repairBetAmountTotal
-     */
-    @ApiModelProperty(value = "补单输赢合计", example = "1500.00")
-    private BigDecimal repairBetAmount = BigDecimal.ZERO;
+    /** proxyNet */
+    @ApiModelProperty(value = "代理商净输赢", example = "1500.00")
+    private BigDecimal proxyNet;
 
-    /**
-     * proxyNetTotal
-     */
-    @ApiModelProperty(value = "代理净输赢CNY", example = "1500.00")
-    private BigDecimal proxyNetCNY = BigDecimal.ZERO;
-    @ApiModelProperty(value = "代理净输赢VND", example = "1500.00")
-    private BigDecimal proxyNetVND = BigDecimal.ZERO;
-    @ApiModelProperty(value = "代理净输赢THB", example = "1500.00")
-    private BigDecimal proxyNetTHB = BigDecimal.ZERO;
+    /** lastMonthBalance */
+    @ApiModelProperty(value = "上月结余", example = "1500.00")
+    private BigDecimal lastMonthBalance;
 
-    /**
-     * lastMonthBalanceTotal
-     */
-    @ApiModelProperty(value = "上月结余CNY", example = "1500.00")
-    private BigDecimal lastMonthBalanceCNY = BigDecimal.ZERO;
-    @ApiModelProperty(value = "上月结余VND", example = "1500.00")
-    private BigDecimal lastMonthBalanceVND = BigDecimal.ZERO;
-    @ApiModelProperty(value = "上月结余THB", example = "1500.00")
-    private BigDecimal lastMonthBalanceTHB = BigDecimal.ZERO;
+    /** rushNet */
+    @ApiModelProperty(value = "冲正后输赢", example = "1500.00")
+    private BigDecimal rushNet;
 
-    /**
-     * rushNetTotal
-     */
-    @ApiModelProperty(value = "冲正净输赢CNY", example = "1500.00")
-    private BigDecimal rushNetCNY = BigDecimal.ZERO;
-    @ApiModelProperty(value = "冲正净输赢VND", example = "1500.00")
-    private BigDecimal rushNetVND = BigDecimal.ZERO;
-    @ApiModelProperty(value = "冲正净输赢THB", example = "1500.00")
-    private BigDecimal rushNetTHB = BigDecimal.ZERO;
+    /** directBetAmount */
+    @ApiModelProperty(value = "直属会员总投注", example = "1500.00")
+    private BigDecimal directBetAmount;
 
-    /**
-     * directBetAmountTotal
-     */
-    @ApiModelProperty(value = "直属会员总投注CNY", example = "1500.00")
-    private BigDecimal directBetAmountCNY = BigDecimal.ZERO;
-    @ApiModelProperty(value = "直属会员总投注VND", example = "1500.00")
-    private BigDecimal directBetAmountVND = BigDecimal.ZERO;
-    @ApiModelProperty(value = "直属会员总投注THB", example = "1500.00")
-    private BigDecimal directBetAmountTHB = BigDecimal.ZERO;
+    /** directValidBetAmount */
+    @ApiModelProperty(value = "直属会员有效投注", example = "1500.00")
+    private BigDecimal directValidBetAmount;
 
-    /**
-     * directValidBetAmountTotal
-     */
-    @ApiModelProperty(value = "直属会员有效投注CNY", example = "1500.00")
-    private BigDecimal directValidBetAmountCNY = BigDecimal.ZERO;
-    @ApiModelProperty(value = "直属会员有效投注VND", example = "1500.00")
-    private BigDecimal directValidBetAmountVND = BigDecimal.ZERO;
-    @ApiModelProperty(value = "直属会员有效投注THB", example = "1500.00")
-    private BigDecimal directValidBetAmountTHB = BigDecimal.ZERO;
+    /** directNetAmount */
+    @ApiModelProperty(value = "直属会员总输赢/投注盈亏", example = "1500.00")
+    private BigDecimal directNetAmount;
 
-    /**
-     * directNetAmountTotal
-     */
-    @ApiModelProperty(value = "直属会员投注盈亏CNY", example = "1500.00")
-    private BigDecimal directNetAmountCNY = BigDecimal.ZERO;
-    @ApiModelProperty(value = "直属会员投注盈亏VND", example = "1500.00")
-    private BigDecimal directNetAmountVND = BigDecimal.ZERO;
-    @ApiModelProperty(value = "直属会员投注盈亏THB", example = "1500.00")
-    private BigDecimal directNetAmountTHB = BigDecimal.ZERO;
-
-    /**
-     * directPayAmountTotal
-     */
-    @ApiModelProperty(value = "直属会员净盈亏CNY", example = "1500.00")
-    private BigDecimal directProfitAmountCNY = BigDecimal.ZERO;
-    @ApiModelProperty(value = "直属会员净盈亏VND", example = "1500.00")
-    private BigDecimal directProfitAmountVND = BigDecimal.ZERO;
-    @ApiModelProperty(value = "直属会员净盈亏THB", example = "1500.00")
-    private BigDecimal directProfitAmountTHB = BigDecimal.ZERO;
+    /** directPayAmount */
+    @ApiModelProperty(value = "直属会员净输赢", example = "1500.00")
+    private BigDecimal directProfitAmount;
 
 }
