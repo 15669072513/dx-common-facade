96ea8ce00b6ea24045f56f25d7b39ccb2a8aaaad
cowboy <cowboy@gmail.com>
2023-06-27 19:54:23 +0800

    牌桌导出功能完善

--- src/main/java/com/dx/facade/constant/ExcelHeadI18nKey.java
+++ src/main/java/com/dx/facade/constant/ExcelHeadI18nKey.java
@@ -446,8 +446,19 @@ public interface ExcelHeadI18nKey {
     String CLUB_PROXY_USERNAME = "EXCEL_HEAD_I18N_KEY.CLUB_代理账号";
     String CLUB_PROXY_REALNAME = "EXCEL_HEAD_I18N_KEY.CLUB_代理名称";
     String CLUB_TOP_PROXY_USERNAME = "EXCEL_HEAD_I18N_KEY.CLUB_所属总代账号";
-    String CLUB_CREATE_AT = "EXCEL_HEAD_I18N_KEY.CLUB_创建时间";
-    
+    String CLUB_CREATE_AT = "`EXCEL_HEAD_I18N_KEY.CLUB_创建时间`";
+
+    String FUNDS_开始时间 = "EXCEL_HEAD_I18N_KEY.FUNDS_开始时间";
+    String FUNDS_结束时间 = "EXCEL_HEAD_I18N_KEY.FUNDS_结束时间";
+    String FUNDS_盲注 = "EXCEL_HEAD_I18N_KEY.FUNDS_盲注";
+    String FUNDS_牌桌手牌数 = "EXCEL_HEAD_I18N_KEY.FUNDS_牌桌手牌数";
+    String FUNDS_玩家总带入 = "EXCEL_HEAD_I18N_KEY.FUNDS_玩家总带入";
+    String FUNDS_玩家总带出 = "EXCEL_HEAD_I18N_KEY.FUNDS_玩家总带出";
+    String FUNDS_总打牌抽水 = "EXCEL_HEAD_I18N_KEY.FUNDS_总打牌抽水";
+    String FUNDS_牌桌盈亏 = "EXCEL_HEAD_I18N_KEY.FUNDS_牌桌盈亏";
+    String FUNDS_牌桌总费用 = "EXCEL_HEAD_I18N_KEY.FUNDS_牌桌总费用";
+    String FUNDS_状态 = "EXCEL_HEAD_I18N_KEY.FUNDS_状态";
+
 
     String FILE_NAME_域名导入模板 = "EXCEL_HEAD_I18N_KEY.FUNDS_域名导入模板";
 
