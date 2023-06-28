68e7cd7aa24be6d59dba0f5a1d4466ebf842f593
two <>
2023-06-28 14:34:16 +0800

    添加代理详情.

--- src/main/java/com/dx/facade/report/service/ProxyRebateRPCService.java
+++ src/main/java/com/dx/facade/report/service/ProxyRebateRPCService.java
@@ -7,49 +7,59 @@ import com.dx.entity.PageResp;
 import com.dx.exception.BizException;
 import com.dx.facade.report.param.ReportRebateReqDTO;
 import com.dx.facade.report.req.proxyrebate.ProxyRebateUnReceiveReqDTO;
+import com.dx.facade.report.resp.ReportRebateDetailsRespDTO;
 import com.dx.facade.report.resp.ReportRebateRespDTO;
 import com.dx.facade.report.resp.ReportRebateTotalRespDTO;
 
 /**
+ * @author gollum
+ * @version 1.0.0
  * @title 返点报表RPC业务接口
  * @description OB旗舰中控后台->报表->业务报表->返点报表
  * /report/previous-rebate/queryRebateList	ProxyRebateRPCService().queryRebateList(pageReq);
  * /report/previous-rebate/queryRebateReportTotal	ProxyRebateRPCService().queryRebateReportTotal(reportRebateReqDTO);
- *
- * @author gollum
- * @version 1.0.0
  * @since 2021年9月18日 上午00:02:38
  */
 public interface ProxyRebateRPCService {
 
     /**
-     * @title 返点报表查询接口s
-     * @author gollum
-     * @since 2021年9月18日 上午00:12:02
      * @param reqDTO
      * @return
      * @throws BizException
+     * @title 返点报表查询接口s
+     * @author gollum
+     * @since 2021年9月18日 上午00:12:02
      */
     CommonResp<PageResp<ReportRebateRespDTO, ?>> queryRebateList(PageReq<ReportRebateReqDTO> reqDTO) throws BizException;
 
     /**
-     * @title 返点报表查询合计接口
-     * @author gollum
-     * @since 2021年9月18日 上午00:44:14
      * @param reqDTO
      * @return
      * @throws BizException
+     * @title 返点报表查询接口s
+     * @author gollum
+     * @since 2021年9月18日 上午00:12:02
+     */
+    CommonResp<PageResp<ReportRebateDetailsRespDTO, ?>> queryRebateDetailList(PageReq<ReportRebateReqDTO> reqDTO) throws BizException;
+
+    /**
+     * @param reqDTO
+     * @return
+     * @throws BizException
+     * @title 返点报表查询合计接口
+     * @author gollum
+     * @since 2021年9月18日 上午00:44:14
      */
     CommonResp<ReportRebateTotalRespDTO> queryRebateReportTotal(ReportRebateReqDTO reqDTO) throws BizException;
 
     /**
      * 待返点条数总计
      *
-     * @author kygo
-     * @since 2022年10月19日 下午18:18:52
      * @param reqDTO
      * @return CommonResp
      * @throws BizException
+     * @author kygo
+     * @since 2022年10月19日 下午18:18:52
      */
     CommonResp<Integer> proxyRebateUnReceiveCount(ProxyRebateUnReceiveReqDTO reqDTO) throws BizException;
 }
