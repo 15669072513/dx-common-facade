48e8f2787b85e90f2500422b5055a6c1426233bc
maniac <maniac@gmail.com>
2023-07-17 19:48:28 +0800

    feature:会员牌桌列表接口
    # Conflicts:
    #	src/main/java/com/dx/facade/es/service/IGameTableRpcService.java

---
+++ src/main/java/com/dx/facade/es/service/IGameTableRpcService.java
@@ -0,0 +1,23 @@
+package com.dx.facade.es.service;
+
+import com.dx.entity.CommonResp;
+import com.dx.entity.PageResp;
+import com.dx.facade.es.req.GameTableInfoListReq;
+import com.dx.facade.es.resp.GameTableInfoListEsResp;
+import com.dx.facade.texas.req.DxHandInfoListReq;
+import com.dx.facade.texas.resp.DxHandInfoListEsResp;
+
+/**
+ * 牌桌ESRPC接口定义
+ */
+public interface IGameTableRpcService {
+
+
+    /**
+     * 根据条件查询牌桌列表信息-翻页tijiao
+     *
+     * @param req
+     * @return
+     */
+    CommonResp<PageResp<GameTableInfoListEsResp, Void>> listPage(GameTableInfoListReq req);
+}
