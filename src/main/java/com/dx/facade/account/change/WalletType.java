255b1ce82f68c198a1f694b79906d01bb9ed6e0e
Rock <>
2023-08-01 23:46:49 +0800

    修改冲突

--- src/main/java/com/dx/facade/account/change/WalletType.java
+++ src/main/java/com/dx/facade/account/change/WalletType.java
@@ -11,6 +11,8 @@ public enum WalletType implements IWalletType {
     彩金钱包(2, "彩金钱包"),
     虚拟货币(3, "虚拟货币"),
     会员积分(4, "会员积分"),
+
+    //member_lock( 5, "会员提款锁定钱包"),
     cash_lock(5, "中心钱包冻结钱包"),
     commission(6, "佣金钱包"),
     quota(7, "额度钱包"),
