61a88e2b64c96979b37013112042695395c84c0d
buyin <buyin@dx.com>
2023-08-03 12:59:49 +0800

    新增上分RPC

--- src/main/java/com/dx/facade/game/req/UpScoreReq.java
+++ src/main/java/com/dx/facade/game/req/UpScoreReq.java
@@ -1,5 +1,6 @@
 package com.dx.facade.game.req;
 
+import com.dx.enums.ChipScoreEnum;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
@@ -18,4 +19,46 @@ public class UpScoreReq{
     private Long userId;
     @ApiModelProperty("金额")
     private BigDecimal amount;
+
+    /**
+     * 账号名
+     */
+    private String userName;
+
+    /**
+     * 钱包类型
+     */
+    private Integer walletType;
+    /**
+     * 俱乐部
+     */
+    private Long clubId;
+
+    /**
+     * 商户ID
+     */
+    private Long merchantId;
+
+    /**
+     * 商户名称
+     */
+    private String merchantName;
+
+    /**
+     * 昵称
+     */
+    private String nickName;
+    /**
+     * 币种 USDT
+     */
+    private String currency;
+    /**
+     * 事件ID
+     */
+    private String eventId;
+
+    /**
+     * 事件Time
+     */
+    private Long eventTime;
 }
