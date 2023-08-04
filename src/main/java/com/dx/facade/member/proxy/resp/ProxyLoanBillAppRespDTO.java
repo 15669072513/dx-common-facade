c3ef3ae18a0c53fc8b55f7e18c6f994fe8915fc9
burn <channel@gmaeil.com>
2023-08-04 15:47:31 +0800

    api接口定义

--- src/main/java/com/dx/facade/member/proxy/resp/ProxyLoanBillAppRespDTO.java
+++ src/main/java/com/dx/facade/member/proxy/resp/ProxyLoanBillAppRespDTO.java
@@ -12,9 +12,21 @@ public class ProxyLoanBillAppRespDTO {
     @ApiModelProperty(value = "账单结束日期")
     private String periodEndDate;
 
-    @ApiModelProperty("中心钱包余额")
-    private BigDecimal walletBalance;
+    @ApiModelProperty("净资产")
+    private BigDecimal netAssets;
 
     @ApiModelProperty("应还借款")
     private BigDecimal returnLoan;
+
+    @ApiModelProperty("可提现额")
+    private BigDecimal withdrawable;
+
+    @ApiModelProperty("中心钱包余额")
+    private BigDecimal walletBalance;
+
+    @ApiModelProperty("信用余额")
+    private BigDecimal creditBalance;
+
+    @ApiModelProperty("授信额度")
+    private BigDecimal creditQuota;
 }
