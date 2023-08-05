33acd0c4f54473ed3c8427e70d3d1a4b9f69c7e0
broadway <broadway@dzpk.onmicrosoft.com>
2023-08-05 19:22:27 +0800

    游戏个人信息 rpc 接口

---
+++ src/main/java/com/dx/facade/texas/service/IDxGamePersonalService.java
@@ -0,0 +1,15 @@
+package com.dx.facade.texas.service;
+
+import com.dx.entity.CommonResp;
+import com.dx.facade.texas.req.DxGameConfigListReq;
+import com.dx.facade.texas.req.DxGamePersonalReq;
+import com.dx.facade.texas.resp.DxGamePersonalResp;
+
+public interface IDxGamePersonalService {
+    /**
+     * 根据用户ID、牌桌类型获取游戏个人信息
+     * @param dxGamePersonalReq
+     * @return
+     */
+    CommonResp<DxGamePersonalResp> getGamePersonalInfo(DxGamePersonalReq dxGamePersonalReq);
+}
