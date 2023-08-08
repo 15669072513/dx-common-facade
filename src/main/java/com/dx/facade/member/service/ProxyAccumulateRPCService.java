cd69ee4e6a8aaf1a16d30a0342ad067fefe2ede1
dealer <dealer@dzpk.onmicrosoft.com>
2023-08-08 21:31:51 +0800

    v2版 代理财务统计信息 编码完成，还差一个中控和代理后台各完成一个接口

--- src/main/java/com/dx/facade/member/service/ProxyAccumulateRPCService.java
+++ src/main/java/com/dx/facade/member/service/ProxyAccumulateRPCService.java
@@ -1,36 +1,32 @@
 package com.dx.facade.member.service;
 
 
+import com.dx.exception.BizException;
+import com.dx.facade.member.param.ProxyAccumulateParamDTO;
+import io.swagger.annotations.ApiModelProperty;
+
 /**
  * 代理财务信息-v2 业务RPC接口
  */
 public interface ProxyAccumulateRPCService {
 	
-	//
-	///**
-	// * 新增或更新代理财务信息
-	// *
-	// * @param reqDto 新增更新代理财务信息入参
-	// * @return 受影响行数
-	// * @throws BizException 异常
-	// */
-	//@ApiModelProperty("新增或更新代理财务信息")
-    //boolean insertOrUpdate(ProxyAccumulateParamDTO reqDto) throws BizException;
-	///**
-	// * 通过代理id获取代理财务信息
-	// *
-	// * @param proxyId 代理id
-	// * @return 代理财务信息
-	// */
-	//@ApiModelProperty("通过代理id获取代理财务信息")
-	//ProxyAccumulateParamDTO selectByProxyId(Long proxyId);
-	//
-	///**
-	// * 根据累计获取佣金范围或者累计返点范围查询代理集合
-	// *
-	// * @param record 代理ID
-	// * @param suffix 分表前缀
-	// * @return 代理财务信息
-	// */
-	//CommonResp<List<Long>> selectByTotalReceive(@Param("record") ProxyDayReportReqDTO record, @Param("suffix") Long suffix);
+
+	/**
+	 * 新增或更新代理财务信息
+	 *
+	 * @param reqDto 新增更新代理财务信息入参
+	 * @return 受影响行数
+	 * @throws BizException 异常
+	 */
+	@ApiModelProperty("新增或更新代理财务信息")
+    boolean insertOrUpdate(ProxyAccumulateParamDTO reqDto) throws BizException;
+
+	/**
+	 * 通过代理id获取代理财务信息
+	 *
+	 * @param proxyId 代理id
+	 * @return 代理财务信息
+	 */
+	@ApiModelProperty("通过代理id获取代理财务信息")
+	ProxyAccumulateParamDTO selectByProxyId(Long proxyId);
 }
