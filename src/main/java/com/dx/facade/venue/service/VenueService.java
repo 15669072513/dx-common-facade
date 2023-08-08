cd63eb3a80eab8e522f7d8e56784e7e976b39d62
Rock <>
2023-08-09 00:25:50 +0800

    修复已知问题

--- src/main/java/com/dx/facade/venue/service/VenueService.java
+++ src/main/java/com/dx/facade/venue/service/VenueService.java
@@ -1,10 +1,26 @@
 package com.dx.facade.venue.service;
 
+import com.dx.entity.CommonResp;
+import com.dx.facade.venue.dto.GameHallDTO;
+import com.dx.facade.venue.dto.GameVenueDTO;
+
+import java.util.List;
+
 /**
  * @Author Rock
  * 场馆业务RPC
  **/
 public interface VenueService {
+
+    CommonResp<GameHallDTO> venueList();
+
+
+    CommonResp<List<GameVenueDTO>> recommendGameList(String venueTypeCode);
+
+
+    CommonResp<List<GameVenueDTO>> switchGameList();
+
+
 
 
 }
