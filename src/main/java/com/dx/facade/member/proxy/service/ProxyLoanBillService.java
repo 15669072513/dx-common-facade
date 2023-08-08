3f812778c2a580f429f849032deeadeb168cc51f
burn <channel@gmaeil.com>
2023-08-08 10:34:25 +0800

    api接口

--- src/main/java/com/dx/facade/member/proxy/service/ProxyLoanBillService.java
+++ src/main/java/com/dx/facade/member/proxy/service/ProxyLoanBillService.java
@@ -4,7 +4,11 @@ import com.dx.entity.CommonResp;
 import com.dx.entity.PageReq;
 import com.dx.entity.PageResp;
 import com.dx.facade.member.proxy.req.ProxyLoanBillAppReqDTO;
+import com.dx.facade.member.proxy.req.ProxyLoanBillUnPubBatchReqDTO;
 import com.dx.facade.member.proxy.resp.ProxyLoanBillAppRespDTO;
+import com.dx.facade.member.proxy.resp.ProxyLoanBillUnPubBatchRespDTO;
+
+import java.util.List;
 
 
 /**
@@ -15,4 +19,6 @@ public interface ProxyLoanBillService {
      * 代理借款账单 for app
      */
     CommonResp<PageResp<ProxyLoanBillAppRespDTO, ?>> queryProxyLoanBillAppPage(PageReq<ProxyLoanBillAppReqDTO> reqDTO);
+
+    CommonResp<List<ProxyLoanBillUnPubBatchRespDTO>> queryUnPubBatch(ProxyLoanBillUnPubBatchReqDTO reqDTO);
 }
