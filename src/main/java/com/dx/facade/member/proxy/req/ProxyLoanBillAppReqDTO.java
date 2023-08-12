802134545ed21f2778f24fceb67fe8526449cc3e
dealer <dealer@dzpk.onmicrosoft.com>
2023-08-12 10:51:06 +0800

    调整未读账单数查询接口参数

--- src/main/java/com/dx/facade/member/proxy/req/ProxyLoanBillAppReqDTO.java
+++ src/main/java/com/dx/facade/member/proxy/req/ProxyLoanBillAppReqDTO.java
@@ -6,12 +6,12 @@ import lombok.Data;
 @Data
 public class ProxyLoanBillAppReqDTO {
 
-    @ApiModelProperty(value = "商户id")
+    @ApiModelProperty(value = "商户id", required = true)
     private Long merchantId;
 
-    @ApiModelProperty(value = "会员id")
+    @ApiModelProperty(value = "会员id-如果当前用户是会员则填充该值")
     private Long memberId;
 
-    @ApiModelProperty(value = "代理id")
+    @ApiModelProperty(value = "代理id-如果当前用户是代理则填充该值")
     private Long proxyId;
 }
