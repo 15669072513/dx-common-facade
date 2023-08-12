028a09ba8711354caf04b6b727e38c742e99d41a
dealer <dealer@dzpk.onmicrosoft.com>
2023-08-12 10:41:04 +0800

    调整未读账单数查询接口参数

--- src/main/java/com/dx/facade/member/proxy/req/ProxyLoanBillAppReqDTO.java
+++ src/main/java/com/dx/facade/member/proxy/req/ProxyLoanBillAppReqDTO.java
@@ -1,11 +1,10 @@
 package com.dx.facade.member.proxy.req;
 
-import com.dx.facade.report.param.base.LocalDatePageRequest;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 @Data
-public class ProxyLoanBillAppReqDTO extends LocalDatePageRequest {
+public class ProxyLoanBillAppReqDTO {
 
     @ApiModelProperty(value = "商户id")
     private Long merchantId;
