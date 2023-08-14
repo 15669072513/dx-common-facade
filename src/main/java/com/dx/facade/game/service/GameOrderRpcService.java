b54c9ec4d831bfc4eae2c3b40e54c2fd93b675c2
wuyanzu01 <2524173458@qq.com>
2023-08-14 22:08:18 +0800

    更改资金明细页返回的id

--- src/main/java/com/dx/facade/game/service/GameOrderRpcService.java
+++ src/main/java/com/dx/facade/game/service/GameOrderRpcService.java
@@ -3,7 +3,14 @@ package com.dx.facade.game.service;
 import com.dx.entity.CommonResp;
 import com.dx.entity.PageResp;
 import com.dx.facade.game.dto.GameOrderRecordDto;
+import com.dx.facade.game.dto.game.UserValiBetAmountRespDto;
+import com.dx.facade.game.req.GameOrderRecordUserVenueReq;
+import com.dx.facade.game.req.GameOrderUserReq;
 import com.dx.facade.game.req.GameReviewReq;
+import com.dx.facade.game.req.UserValiBetAmountReq;
+import com.dx.facade.game.resp.GameOrderRecordUserVenueResp;
+import com.dx.facade.game.resp.GameOrderUserResp;
+import com.dx.facade.game.resp.GameOrderUserSumResp;
 import com.dx.facade.game.resp.GameRecordDeatailPackResp;
 import com.dx.facade.game.resp.GameReviecardRoundResp;
 import com.dx.facade.game.resp.GameReviewcardResp;
@@ -70,5 +77,24 @@ public interface GameOrderRpcService {
      */
     CommonResp<PageResp<GameRecordDeatailPackResp, Void>> getGameReviewDeatailList(GameReviewReq req);
 
+    /**
+     * 用户投注记录
+     * @param req
+     * @return
+     */
+    CommonResp<PageResp<GameOrderUserResp, GameOrderUserSumResp>> gameOrderUserList(GameOrderUserReq req);
 
+    /**
+     * 场馆维度用户记录
+     * @param req
+     * @return
+     */
+    CommonResp<PageResp<GameOrderRecordUserVenueResp, ?>> gameOrderUserVenueList(GameOrderRecordUserVenueReq req);
+
+    /**
+     * 用户有效投注总计
+     * @param req
+     * @return
+     */
+    CommonResp<UserValiBetAmountRespDto> userVlidBetAmountSum(UserValiBetAmountReq req);
 }
