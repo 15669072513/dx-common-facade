4c09dd386aa708bcba120a44607cc5496768f53d
moka <justgogoo@outlook.com>
2023-08-14 09:35:12 +0800

    迁移代码

--- src/main/java/com/dx/facade/game/service/GameOrderRecordRpcService.java
+++ src/main/java/com/dx/facade/game/service/GameOrderRecordRpcService.java
@@ -4,32 +4,37 @@ import com.dx.entity.CommonResp;
 import com.dx.entity.PageResp;
 import com.dx.facade.game.req.FundDetailReq;
 import com.dx.facade.game.req.GameOrderRecordProxyReq;
+import com.dx.facade.game.req.GameOrderTableDetailSumReq;
 import com.dx.facade.game.resp.FundDetailRecordResp;
 import com.dx.facade.game.resp.FundDetailSumResp;
 import com.dx.facade.game.resp.GameOrderRecordProxyResp;
+import com.dx.facade.game.resp.gamedetail.GameOrderRecordDetail;
 
+/**
+ * 战绩
+ * @author broadway
+ */
 public interface GameOrderRecordRpcService {
+    /**
+     * 查询战绩详情
+     * @param req
+     * @return
+     */
+    CommonResp<PageResp<GameOrderRecordDetail.GameOrderRecord, GameOrderRecordDetail.GameOrderRecordSummary>> queryGameOrderRecordDetailInfo(GameOrderTableDetailSumReq req);
 
+    /**
+     * 资金明细页的统计数据
+     * @param req
+     * @return
+     */
+    CommonResp<FundDetailSumResp> getFundDetailSum(FundDetailReq req);
 
-  /**
-   * 资金明细页的统计数据
-   * @param req
-   * @return
-   */
-  CommonResp<FundDetailSumResp> getFundDetailSum(FundDetailReq req);
+    /**
+     * 资金明细页分页数据
+     * @param req
+     * @return
+     */
+    CommonResp<PageResp<FundDetailRecordResp,Void>> getFundDetailRecord(FundDetailReq req);
 
-  /**
-   * 资金明细页分页数据
-   * @param req
-   * @return
-   */
-  CommonResp<PageResp<FundDetailRecordResp,Void>> getFundDetailRecord(FundDetailReq req);
-
-  /**
-   * 游戏记录分页数据
-   * @param req
-   * @return
-   */
-  CommonResp<PageResp<GameOrderRecordProxyResp, ?>> getGameRecord(GameOrderRecordProxyReq req);
-
+    CommonResp<PageResp<GameOrderRecordProxyResp, ?>> getGameRecord(GameOrderRecordProxyReq req);
 }
