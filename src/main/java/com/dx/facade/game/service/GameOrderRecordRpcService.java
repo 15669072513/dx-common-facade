24b43091a145b42781c3f75c3dc896a39f27ad84
wuyanzu01 <2524173458@qq.com>
2023-08-14 10:58:11 +0800

    资金明细页rpc接口

---
+++ src/main/java/com/dx/facade/game/service/GameOrderRecordRpcService.java
@@ -0,0 +1,25 @@
+package com.dx.facade.game.service;
+
+import com.dx.entity.CommonResp;
+import com.dx.entity.PageResp;
+import com.dx.facade.game.req.FundDetailReq;
+import com.dx.facade.game.resp.FundDetailRecordResp;
+import com.dx.facade.game.resp.FundDetailSumResp;
+
+public interface GameOrderRecordRpcService {
+
+
+  /**
+   * 资金明细页的统计数据
+   * @param req
+   * @return
+   */
+  CommonResp<FundDetailSumResp> getFundDetailSum(FundDetailReq req);
+
+  /**
+   * 资金明细页分页数据
+   * @param req
+   * @return
+   */
+  CommonResp<PageResp<FundDetailRecordResp,Void>> getFundDetailRecord(FundDetailReq req);
+}
