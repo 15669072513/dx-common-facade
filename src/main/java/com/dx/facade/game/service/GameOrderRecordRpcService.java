22a58365bd29f215585d6d95f909593151d82d72
moka <justgogoo@outlook.com>
2023-08-14 11:05:44 +0800

    游戏记录RPC

--- src/main/java/com/dx/facade/game/service/GameOrderRecordRpcService.java
+++ src/main/java/com/dx/facade/game/service/GameOrderRecordRpcService.java
@@ -3,8 +3,10 @@ package com.dx.facade.game.service;
 import com.dx.entity.CommonResp;
 import com.dx.entity.PageResp;
 import com.dx.facade.game.req.FundDetailReq;
+import com.dx.facade.game.req.GameOrderRecordProxyReq;
 import com.dx.facade.game.resp.FundDetailRecordResp;
 import com.dx.facade.game.resp.FundDetailSumResp;
+import com.dx.facade.game.resp.GameOrderRecordProxyResp;
 
 public interface GameOrderRecordRpcService {
 
@@ -22,4 +24,12 @@ public interface GameOrderRecordRpcService {
    * @return
    */
   CommonResp<PageResp<FundDetailRecordResp,Void>> getFundDetailRecord(FundDetailReq req);
+
+  /**
+   * 游戏记录分页数据
+   * @param req
+   * @return
+   */
+  CommonResp<PageResp<GameOrderRecordProxyResp, ?>> getGameRecord(GameOrderRecordProxyReq req);
+
 }
