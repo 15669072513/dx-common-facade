9e66ca5dddf9c17403ae22095e394b6d9eb55d1a
Middle <middle@gmail.com>
2023-08-16 14:19:17 +0800

    fix

--- src/main/java/com/dx/facade/account/change/WalletType.java
+++ src/main/java/com/dx/facade/account/change/WalletType.java
@@ -12,7 +12,7 @@ public enum WalletType implements IWalletType {
     虚拟货币(3, "虚拟货币"),
     会员积分(4, "会员积分"),
 
-    //member_lock( 5, "会员提款锁定钱包"),
+    member_lock( 5, "会员提款锁定钱包"),
     cash_lock(5, "中心钱包冻结钱包"),
     commission(6, "佣金钱包"),
     quota(7, "额度钱包"),
