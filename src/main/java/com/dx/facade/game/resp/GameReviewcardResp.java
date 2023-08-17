50b76017eba8bbb3af8c41aeeab286979b70a29d
streak <streak@dzpk.onmicrosoft.com>
2023-08-17 15:11:53 +0800

    解决合并冲突

--- src/main/java/com/dx/facade/game/resp/GameReviewcardResp.java
+++ src/main/java/com/dx/facade/game/resp/GameReviewcardResp.java
@@ -85,7 +85,6 @@ public class GameReviewcardResp implements Serializable  {
     private String lastCards;
 
     /** 成牌时7张牌舍弃的2张牌 */
-    @JsonIgnore
     @ApiModelProperty("成牌时7张牌舍弃的2张牌")
     @JsonIgnore
     private String loseCards;
