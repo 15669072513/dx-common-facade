7b9e534a814acf5e83da585d11ea1f0fb37d9f3b
Rock <rock@dzpk.onmicrosoft.com>
2023-08-18 19:35:57 +0800

    新增TAG

--- src/main/java/com/dx/facade/venue/constant/VenueTopicConstant.java
+++ src/main/java/com/dx/facade/venue/constant/VenueTopicConstant.java
@@ -4,4 +4,6 @@ public interface VenueTopicConstant {
 
     String TEST_VENUE_GAME_RECORD_TOPIC = "test-venue-game-record-topic";
 
+    String TEST_VENUE_GAME_RECORD_TAG = "testVenueGameRecordTag";
+
 }
