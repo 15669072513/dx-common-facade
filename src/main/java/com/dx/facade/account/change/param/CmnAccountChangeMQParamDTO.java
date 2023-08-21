a50a794e65171771b992cf56a2d77d95c2180653
dealer <dealer@dzpk.onmicrosoft.com>
2023-08-21 21:05:14 +0800

    acount-service 账变通用处理逻辑完善

--- src/main/java/com/dx/facade/account/change/param/CmnAccountChangeMQParamDTO.java
+++ src/main/java/com/dx/facade/account/change/param/CmnAccountChangeMQParamDTO.java
@@ -1,6 +1,7 @@
 package com.dx.facade.account.change.param;
 
 import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -15,17 +16,33 @@ import java.io.Serializable;
  * @date 2023/08/21
  * @copyright
  */
-@Slf4j
 @Data
+@Slf4j
 @SuperBuilder
 @NoArgsConstructor
 @AllArgsConstructor
-@ApiModel(value = "通用mq账变对象", description = "通用mq账变对象")
+@ApiModel(value="通用mq账变对象", description="通用mq账变对象")
 public class CmnAccountChangeMQParamDTO implements Serializable {
 
-    private IBWWalletChangeParm changeParm;
+    @ApiModelProperty(value = "OBQJ会员账变且计算流水")
+    private MemberBillWalletChangeParmDTO memberBillWalletChangeParmDTO;
 
-    //@See com.dx.facade.account.change.param.CmnAccountChangeMQParamType.code
-    private Integer paramType;
+    @ApiModelProperty(value = "会员账变对象(退还筹码为0)")
+    private MemberWalletChangeInZeroParmDTO memberWalletChangeInZeroParmDTO;
+
+    @ApiModelProperty(value = "会员账变对象")
+    private MemberWalletChangeParmDTO memberWalletChangeParmDTO;
+
+    @ApiModelProperty(value = "同一事务下同一用户多次账变")
+    private MultipleWalletChangeParmDTO multipleWalletChangeParmDTO;
+
+    @ApiModelProperty(value = "代理账变对象")
+    private ProxyBillWalletChangeParmDTO proxyBillWalletChangeParmDTO;
+
+    @ApiModelProperty(value = "代理账变且计算流水(代理目前只有私庄业务才计流水)")
+    private ProxyWalletChangeParmDTO proxyWalletChangeParmDTO;
+
+    @ApiModelProperty(value = "转账行为导致的账变对象(包含主动方和被动方)")
+    private TransferWalletChangeParmDTO transferWalletChangeParmDTO;
 
 }
