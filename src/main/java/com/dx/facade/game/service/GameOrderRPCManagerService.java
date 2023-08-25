32512d471e5202d24c753577768b9be5e66e7b92
streak <streak@dzpk.onmicrosoft.com>
2023-08-25 11:46:57 +0800

    提交es重构的rpc接口

---
+++ src/main/java/com/dx/facade/game/service/GameOrderRPCManagerService.java
@@ -0,0 +1,35 @@
+package com.dx.facade.game.service;
+
+import com.dx.entity.CommonResp;
+import com.dx.entity.PageResp;
+import com.dx.facade.game.dto.game.*;
+import com.dx.facade.game.req.*;
+import com.dx.facade.game.resp.GameOrderClubSumResp;
+
+public interface GameOrderRPCManagerService {
+    /**
+     *  综合后台--手牌 金额相关汇总
+     */
+    CommonResp<GameOrderHandSumRespDto> gameOrderHandSumList(GameOrderHandSumReq req);
+    /**
+     *  综合后台--牌桌 金额相关汇总
+     */
+    CommonResp<GameOrderTableSumRespDto> gameOrderTableSumList(GameOrderTableSumReq req);
+
+    /**
+     * 手牌下面的玩家列表
+     */
+    CommonResp<GameOrderRecordUserHandRespDto> gameOrderRecordUserHandList(GameOrderRecordUserHandReq req);
+
+
+    /**
+     * 综合后天-俱乐部 金额相关汇总
+     */
+    CommonResp<GameOrderClubSumRespDto> gameOrderClubSumList(GameOrderClubSumReq req);
+
+    /**
+     *  俱乐部贡献
+     */
+    CommonResp<PageResp<GameOrderClubSumResp, ?>> gameOrderClubContributeList(GameOrderClubSumReq req);
+
+}
