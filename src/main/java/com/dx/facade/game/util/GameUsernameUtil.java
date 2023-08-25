152b94cb85ea22e79bff1461e763f996f7fa6215
maniac <maniac@gmail.com>
2023-08-25 20:41:36 +0800

    fix：dev dev1测试三方玩家名称重复问题

--- src/main/java/com/dx/facade/game/util/GameUsernameUtil.java
+++ src/main/java/com/dx/facade/game/util/GameUsernameUtil.java
@@ -3,12 +3,14 @@ package com.dx.facade.game.util;
 import java.util.Objects;
 
 import com.dx.facade.enums.BwEnvEnum;
+import com.dx.util.StringUtil;
 
 public class GameUsernameUtil {
 
     /**
      * 获取会员在场馆注册的游戏名
-     * @param envEnum 环境信息 dev test prerelease prod
+     *
+     * @param envEnum    环境信息 dev test prerelease prod
      * @param merchantId 商户ID
      * @param memberName 用户注册的名会员名
      * @return 环境信息开头1位 + 商户号尾号4位 + 会员名  如 d4843Abc123456
@@ -21,8 +23,14 @@ public class GameUsernameUtil {
         // if (BwEnvEnum.PROD.getId().equals(envEnum.getId())) {
         // return memberName;
         // } else {
-        return envEnum.getName().substring(0, 1) + merchantId % 10000 + memberName;
-            // }
+        String prefix = "";
+        prefix = envEnum.getName().substring(envEnum.getName().length() - 1, envEnum.getName().length());
+        return prefix + merchantId % 10000 + memberName;
+        // }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(BwEnvEnum.PRE.getName().substring(BwEnvEnum.PRE.getName().length() - 1, BwEnvEnum.PRE.getName().length()) + 111112111201L % 10000 + "maniac");
     }
 
     public static Boolean verifyPlayerName(BwEnvEnum envEnum, Long merchantId, String playName) {
@@ -32,9 +40,9 @@ public class GameUsernameUtil {
 //        if (BwEnvEnum.PROD.getId().equals(envEnum.getId())) {
 //            return true;
 //        } else {
-            String prefix = envEnum.getName().substring(0, 1) + merchantId % 10000;
-            return playName.indexOf(prefix) >= 0;
-            // }
+        String prefix = envEnum.getName().substring(0, 1) + merchantId % 10000;
+        return playName.indexOf(prefix) >= 0;
+        // }
     }
 
 }
