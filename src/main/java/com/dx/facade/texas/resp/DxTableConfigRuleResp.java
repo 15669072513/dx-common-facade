69aeab39a4910926ee6df72ed23c23313d445f1b
broadway <broadway@dzpk.onmicrosoft.com>
2023-08-31 21:09:11 +0800

    俱乐部新加字段同步

--- src/main/java/com/dx/facade/texas/resp/DxTableConfigRuleResp.java
+++ src/main/java/com/dx/facade/texas/resp/DxTableConfigRuleResp.java
@@ -165,4 +165,10 @@ public class DxTableConfigRuleResp implements Serializable {
 
     @ApiModelProperty("时长（小时）")
     private Integer hourTimeCount;
+
+    @ApiModelProperty("德州扑克单牌桌累计带入上限")
+    private Integer texasBringInLimit;
+
+    @ApiModelProperty("短牌单牌桌累计带入上限")
+    private Integer shortCardBringInLimit;
 }
