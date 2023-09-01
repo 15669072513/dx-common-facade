0dd51b82432fc0e1d48f941370dcae65d191ab19
“set” <set@dzpk.onmicrosoft.com>
2023-09-01 11:28:22 +0800

    增加获取用户牌桌最后一条账变记录的账变后的余额rpc

--- src/main/java/com/dx/facade/texas/service/WalletRpcService.java
+++ src/main/java/com/dx/facade/texas/service/WalletRpcService.java
@@ -1,7 +1,9 @@
 package com.dx.facade.texas.service;
 
 import com.dx.entity.CommonResp;
+import com.dx.facade.texas.dto.ChangeAfterAmountDto;
 import com.dx.facade.texas.dto.WalletDto;
+import com.dx.facade.texas.req.ChangeAfterAmountReq;
 import com.dx.facade.texas.req.WalletRequest;
 
 public interface WalletRpcService {
@@ -12,4 +14,11 @@ public interface WalletRpcService {
    * @return
    */
   CommonResp<WalletDto> getUserWallet(WalletRequest request);
+
+  /**
+   * 获取用户牌桌最后一条账变记录的账变后的余额
+   * @param req
+   * @return
+   */
+  CommonResp<ChangeAfterAmountDto> getChangeAfterAmount(ChangeAfterAmountReq req);
 }
