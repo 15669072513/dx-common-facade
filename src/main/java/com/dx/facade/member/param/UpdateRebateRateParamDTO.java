4e1fa87b19b698c703b324d19e770976270f0f4a
bicycle <bicycle@dzpk.onmicrosoft.com>
2023-09-05 15:31:39 +0800

    feat:优化代码

--- src/main/java/com/dx/facade/member/param/UpdateRebateRateParamDTO.java
+++ src/main/java/com/dx/facade/member/param/UpdateRebateRateParamDTO.java
@@ -1,11 +1,7 @@
 package com.dx.facade.member.param;
 
 import java.io.Serializable;
-import java.math.BigDecimal;
 import java.time.LocalDateTime;
-
-import javax.validation.constraints.DecimalMin;
-import javax.validation.constraints.NotNull;
 
 import org.springframework.util.Assert;
 
@@ -18,6 +14,7 @@ import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 /**
  * 更新返点比例请求参数DTO
@@ -26,66 +23,16 @@ import lombok.NoArgsConstructor;
  *
  */
 @Data
+@EqualsAndHashCode(callSuper=false)
 @AllArgsConstructor
 @NoArgsConstructor
 @ApiModel(value = "更新返点比例请求Param", description = "更新返点比例请求Param")
-public class UpdateRebateRateParamDTO  implements BaseParmDTO, Serializable {
+public class UpdateRebateRateParamDTO extends BaseRebateRateParamDTO  implements BaseParmDTO, Serializable {
 
     private static final long serialVersionUID = 1L;
-
+    
     @ApiModelProperty("ID")
     private Long id;
-    
-    @ApiModelProperty("商户Id")
-    private Long merchantId;
-    
-    @ApiModelProperty("用户类型 0：会员 1：代理")
-    private Integer userType;
-    
-    @ApiModelProperty("用户Id")
-    private Long userId;
-
-    @ApiModelProperty("用户名称")
-    private String userName;
-    
-    @ApiModelProperty("上级代理Id")
-    private Long parentProxyId;
-
-    @ApiModelProperty("上级代理名称")
-    private String parentProxyName;
-    
-    @ApiModelProperty("德州返点比例")
-    @DecimalMin(value = "0.01", message = "德州返点比例不能小于0.01")
-    @NotNull(message = "德州返点比例不能为空")
-    private BigDecimal texasRebate;
-    
-    @ApiModelProperty("真人返点比例")
-    @NotNull(message = "真人返点比例不能为空")
-    @DecimalMin(value = "0.01", message = "真人返点比例不能小于0.01")
-    private BigDecimal actualPersonRebate;
-    
-    @ApiModelProperty("体育返点比例")
-    @NotNull(message = "体育返点比例不能为空")
-    @DecimalMin(value = "0.01", message = "体育返点比例不能小于0.01")
-    private BigDecimal sportsRebate;
-    
-    @ApiModelProperty("彩票返点比例")
-    @NotNull(message = "彩票返点比例不能为空")
-    @DecimalMin(value = "0.01", message = "彩票返点比例不能小于0.01")
-    private BigDecimal lotteryTicketRebate;
-    
-    @ApiModelProperty("棋牌返点比例")
-    @NotNull(message = "棋牌返点比例不能为空")
-    @DecimalMin(value = "0.01", message = "棋牌返点比例不能小于0.01")
-    private BigDecimal chessRebate;
-    
-    @ApiModelProperty("电竞返点比例")
-    @NotNull(message = "电竞返点比例不能为空")
-    @DecimalMin(value = "0.01", message = "电竞返点比例不能小于0.01")
-    private BigDecimal esportsRebate;
-    
-    @ApiModelProperty("状态:0-未设置 1-已生效 2-已失效 3-平台调整")
-    private Integer status;
 
     @ApiModelProperty("最后操作人")
     private String updatedBy;
