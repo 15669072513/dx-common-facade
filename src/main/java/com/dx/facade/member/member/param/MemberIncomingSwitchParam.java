a0a96bc4a855c7e607f1d04c31189d50c70e272e
bicycle <bicycle@dzpk.onmicrosoft.com>
2023-09-13 18:16:50 +0800

    feat：去掉无用注解

--- src/main/java/com/dx/facade/member/member/param/MemberIncomingSwitchParam.java
+++ src/main/java/com/dx/facade/member/member/param/MemberIncomingSwitchParam.java
@@ -1,7 +1,5 @@
 package com.dx.facade.member.member.param;
 
-import com.baomidou.mybatisplus.annotation.TableField;
-
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
 import lombok.Setter;
@@ -14,12 +12,13 @@ import lombok.Setter;
 @Setter
 @Getter
 public class MemberIncomingSwitchParam {
+	
     @ApiModelProperty("会员id")
     private Long id;
+    
     @ApiModelProperty("游戏锁定状态 0-未锁定 1-游戏锁定")
     private Integer gameLockStatus;
     
     @ApiModelProperty("入局锁定状态 0-未锁定 1-入局锁定")
-    @TableField("join_game_lock_status")
     private Integer joinGameLockStatus;
 }
