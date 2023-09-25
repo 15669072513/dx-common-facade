3da36e364d33e06a01e8105f8299cf4d59e6eb33
“set” <set@dzpk.onmicrosoft.com>
2023-09-25 15:08:55 +0800

    1

--- src/main/java/com/dx/facade/game/req/UpScoreReq.java
+++ src/main/java/com/dx/facade/game/req/UpScoreReq.java
@@ -20,8 +20,6 @@ public class UpScoreReq{
     private Long tableId;
     @ApiModelProperty("tableName")
     private String tableName;
-    @ApiModelProperty("牌桌code")
-    private String tableCode;
     @ApiModelProperty("玩家id")
     private Long userId;
     @ApiModelProperty("金额")
