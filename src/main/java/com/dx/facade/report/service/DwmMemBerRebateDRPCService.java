455adc33c5b48b4ea8abcb13bf1f1f8090693e5e
barrel <barrel>
2023-10-04 16:27:10 +0800

    会员返点详情修改

--- src/main/java/com/dx/facade/report/service/DwmMemBerRebateDRPCService.java
+++ src/main/java/com/dx/facade/report/service/DwmMemBerRebateDRPCService.java
@@ -4,8 +4,9 @@ package com.dx.facade.report.service;
 import com.dx.entity.CommonResp;
 import com.dx.entity.PageResp;
 import com.dx.exception.BizException;
-import com.dx.facade.member.param.MemberNetAmountReqDTO;
+import com.dx.facade.report.param.rebate.DwdBetMemberRecordDReq;
 import com.dx.facade.report.param.rebate.DwmMemBerRebateDReq;
+import com.dx.facade.report.resp.DwdBetMemberRecordDDTO;
 import com.dx.facade.report.resp.DwmMemBerRebateDDTO;
 
 public interface DwmMemBerRebateDRPCService {
@@ -17,6 +18,10 @@ public interface DwmMemBerRebateDRPCService {
      * @throws BizException
      */
     CommonResp<PageResp<DwmMemBerRebateDDTO, ?>> queryMemberRebateList(DwmMemBerRebateDReq reqDTO) throws BizException;
+
+    CommonResp<PageResp<DwdBetMemberRecordDDTO, ?>> queryMemberRebateDetailList(DwdBetMemberRecordDReq reqDTO) throws BizException;
+
+    CommonResp<DwdBetMemberRecordDDTO> queryMemberRebateDetailSummary(DwdBetMemberRecordDReq reqDTO) throws BizException;
 
 
 }
