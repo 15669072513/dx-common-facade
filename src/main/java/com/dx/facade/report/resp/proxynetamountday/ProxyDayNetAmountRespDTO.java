a58279a260071dfc2b0755c4d48550632e7d886f
maniac <maniac@gmail.com>
2023-10-06 15:28:36 +0800

    fix：代理后台代理盈亏接口

--- src/main/java/com/dx/facade/report/resp/proxynetamountday/ProxyDayNetAmountRespDTO.java
+++ src/main/java/com/dx/facade/report/resp/proxynetamountday/ProxyDayNetAmountRespDTO.java
@@ -23,62 +23,91 @@ import java.math.BigDecimal;
 @AllArgsConstructor
 public class ProxyDayNetAmountRespDTO {
 
-    /** proxyId */
+    /**
+     * proxyId
+     */
     @JsonFormat(shape = JsonFormat.Shape.STRING)
     @ApiModelProperty(value = "代理id", example = "10001")
     private Long proxyId;
 
-    /** proxyName */
+    /**
+     * proxyName
+     */
     @ApiModelProperty(value = "代理账号", example = "daxia2222")
     private String proxyName;
 
-    /** betAmount */
+    /**
+     * betAmount
+     */
     @ApiModelProperty(value = "总投注", example = "1500.00")
     private BigDecimal betAmount;
 
-    /** validBetAmount */
+    /**
+     * validBetAmount
+     */
     @ApiModelProperty(value = "有效投注", example = "1500.00")
     private BigDecimal validBetAmount;
 
-    /** netAmount */
+    /**
+     * netAmount
+     */
     @ApiModelProperty(value = "游戏盈亏", example = "1500.00")
     private BigDecimal netAmount;
-
+    @Deprecated
     /** rebateAmount */
     @ApiModelProperty(value = "会员返水", example = "1500.00")
     private BigDecimal rebateAmount;
-
-    /** rebateAmount */
+    @ApiModelProperty(value = "会员返水", example = "1500.00")
+    private BigDecimal memberRebateAmount;
+    /**
+     * rebateAmount
+     */
     @ApiModelProperty(value = "代理返水", example = "1500.00")
     private BigDecimal proxyRebateAmount;
 
-    /** discountAmount */
+    /**
+     * discountAmount
+     */
     @ApiModelProperty(value = "会员优惠", example = "1500.00")
     private BigDecimal discountAmount;
-
-    /** artificialPatchAmount */
+    @Deprecated
+    /**
+     * artificialPatchAmount
+     */
     @ApiModelProperty(value = "会员账户调整", example = "1500.00")
     private BigDecimal artificialPatchAmount;
+
+
+    @ApiModelProperty(value = "会员账户调整", example = "1500.00")
+    private BigDecimal memberArtificialPatchAmount;
 
     @ApiModelProperty(value = "流水纠正", example = "150.00")
     private BigDecimal flowCorrection;
 
-    @ApiModelProperty(value = "总盈亏", example = "150.00")
-    private BigDecimal profitAmount;
+    @ApiModelProperty(value = "会员净盈亏", example = "150.00")
+    private BigDecimal memberNetWinAmount;
 
-    /** proxyNet */
+    /**
+     * proxyNet
+     */
     @ApiModelProperty(value = "场馆费", example = "1500.00")
     private BigDecimal venueFee;
 
-    /** proxyNet */
+    /**
+     * proxyNet
+     */
     @ApiModelProperty(value = "代净输赢", example = "1500.00")
     private BigDecimal proxyNet;
 
-    /** lastMonthBalance */
+    /**
+     * lastMonthBalance
+     */
     @ApiModelProperty(value = "上月结余", example = "1500.00")
     private BigDecimal lastMonthBalance;
 
-    /** rushNet */
+    /**
+     * rushNet
+     */
     @ApiModelProperty(value = "冲正后净输赢", example = "1500.00")
     private BigDecimal rushNet;
 
@@ -87,4 +116,37 @@ public class ProxyDayNetAmountRespDTO {
 
     @ApiModelProperty("会员总盈亏")
     private BigDecimal totalNetAmount;
+
+
+    @ApiModelProperty("直属投注人数")
+    //@TableField("direct_member_count")
+    private Long directMemberCount;
+
+    @ApiModelProperty("直属注单数")
+    //@TableField("direct_bet_count")
+    private Long directBetCount;
+
+    @ApiModelProperty("直属投注额")
+    //@TableField("direct_bet_amount")
+    private BigDecimal directBetAmount;
+
+    @ApiModelProperty("直属有效投注额")
+    //@TableField("direct_valid_bet_amount")
+    private BigDecimal directValidBetAmount;
+
+    @ApiModelProperty("直属输赢金额")
+    //@TableField("direct_net_amount")
+    private BigDecimal directNetAmount;
+
+
+    @ApiModelProperty("直属返水金额=会员返水")
+    //@TableField("direct_rebate_amount")
+    private BigDecimal directRebateAmount;
+
+    @ApiModelProperty("直属其他调整=人工加减额中的其他调整额")
+    //@TableField("direct_artificial_patch_amount")
+    private BigDecimal directArtificialPatchAmount;
+
+    @ApiModelProperty("直属会员净输赢")
+    private BigDecimal directProfitAmount;
 }
