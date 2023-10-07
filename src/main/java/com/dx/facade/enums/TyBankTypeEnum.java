05b4d5245f4b43285864c78bd2291ba48f558268
flop <flop@dzpk.onmicrosoft.com>
2023-10-07 20:30:21 +0800

    银行相关
    银行相关

---
+++ src/main/java/com/dx/facade/enums/TyBankTypeEnum.java
@@ -0,0 +1,184 @@
+package com.dx.facade.enums;
+
+/**
+ * 支持银行卡
+ */
+public enum TyBankTypeEnum {
+
+    ABC(1L,"ABC", "中国农业银行","农业银行"),
+    ADBC(2L,"ADBC", "农业发展银行","农业发展银行"),
+    AHXABANK(3L,"AHXABANK", "新安银行","新安银行"),
+    AMBANK(4L,"AMBANK", "澳门银行","澳门银行"),
+    AQBANK(5L,"AQBANK", "安庆银行","安庆银行"),
+    ASBANK(6L,"ASBANK", "鞍山银行","鞍山银行"),
+    ASBN(7L,"ASBN", "安顺银行","安顺银行"),
+    AYBANK(8L,"AYBANK", "安阳银行","安阳银行"),
+    BankofDalian(9L,"BankofDalian", "大连银行","大连银行"),
+    BANKWF(10L,"BANKWF", "潍坊银行","潍坊银行"),
+    BCCB(11L,"BCCB", "北京商业银行","北京商业银行"),
+    BCM(12L,"BCM", "交通银行","交通"),
+    BEEB(13L,"BEEB", "鄞州银行","鄞州银行"),
+    BHB(14L,"BHB", "河北银行","河北银行"),
+    BJBANK(15L,"BJBANK", "北京银行","北京"),
+    BJRCB(16L,"BJRCB", "北京农商","北京农商"),
+    BOB(17L,"BOB", "保定银行","保定银行"),
+    BOBZ(18L,"BOBZ", "本溪银行","本溪银行"),
+    BOC(19L,"BOC", "中国银行","中国"),
+    BOCD(20L,"BOCD", "成都商业银行","成都商业银行"),
+    BOCZ(21L,"BOCZ", "沧州银行","沧州银行"),
+    BOD(22L,"BOD", "东莞银行","东莞银行"),
+    BODD(23L,"BODD", "丹东银行","丹东银行"),
+    BODZ(24L,"BODZ", "达州银行","达州银行"),
+    BOFX(25L,"BOFX", "阜新银行","阜新银行"),
+    BOIMC(26L,"BOIMC", "内蒙古银行","内蒙古"),
+    BOJN(27L,"BOJN", "济宁银行","济宁银行"),
+    BOJZ(28L,"BOJZ", "锦州银行","锦州银行"),
+    BORF(29L,"BORF", "瑞丰银行","瑞丰银行"),
+    BOSZ(30L,"BOSZ", "苏州银行","苏州银行"),
+    BOXM(31L,"BOXM", "厦门银行","厦门银行"),
+    BOYK(32L,"BOYK", "营口银行","营口银行"),
+    BRCB(33L,"BRCB", "北京农商银行","北京农商银行"),
+    BSB(34L,"BSB", "包商银行","包商银行"),
+    CBHB(35L,"CBHB", "渤海银行","渤海"),
+    CCAB(36L,"CCAB", "长安银行","长安银行"),
+    CCB(37L,"CCB", "中国建设银行","建设"),
+    CDBANK(38L,"CDBANK", "承德银行","承德银行"),
+    CDCB(39L,"CDCB", "成都银行","成都银行"),
+    CEB(40L,"CEB", "中国光大银行","光大"),
+    CGB(41L,"CGB", "广东发展银行","广东发展"),
+    CIB(42L,"CIB", "兴业银行","兴业"),
+    CITIBANK(43L,"CITIBANK", "花旗银行","花旗银行"),
+    CMB(44L,"CMB", "中国招商银行","招商"),
+    CMBC(45L,"CMBC", "中国民生银行","民生"),
+    CNCB(46L,"CNCB", "中信银行","中信"),
+    CQBANK(47L,"CQBANK", "重庆银行","重庆银行"),
+    CSCB(48L,"CSCB", "长沙银行","长沙"),
+    CXBANK(49L,"CXBANK", "创兴银行","创兴银行"),
+    CYCB(50L,"CYCB", "朝阳银行","朝阳银行"),
+    CZBA(51L,"CZBA", "常州银行","常州银行"),
+    CZBANK(52L,"CZBANK", "浙商银行","浙商"),
+    CZCB(53L,"CZCB", "稠州银行","稠州银行"),
+    DFBANK(54L,"DFBANK", "大丰银行","大丰银行"),
+    DYB(55L,"DYB", "德阳银行","德阳银行"),
+    DZBCHINA(11L,"DZBCHINA", "德州银行","德州银行"),
+    EGBANK(11L,"EGBANK", "恒丰银行","恒丰"),
+    FDB(11L,"FDB", "富滇银行","富滇"),
+    FJHXBANK(11L,"FJHXBANK", "海峡银行","海峡银行"),
+    FSCB(11L,"FSCB", "抚顺银行","抚顺银行"),
+    GCB(11L,"GCB", "广州银行","广州"),
+    GLB(11L,"GLB", "桂林银行","桂林银行"),
+    GSBANK(11L,"GSBANK", "甘肃银行","甘肃银行"),
+    GYCB(11L,"GYCB", "贵阳银行","贵阳银行"),
+    GZB(11L,"GZB", "赣州银行","赣州银行"),
+    HABANK(11L,"HABANK", "淮安银行","淮安银行"),
+    HBBAN(11L,"HBBAN", "鹤壁银行","鹤壁银行"),
+    HBBANK(11L,"HBBANK", "淮北银行","淮北银行"),
+    HBC(11L,"HBC", "湖北银行","湖北银行"),
+    HDBANK(11L,"HDBANK", "邯郸银行","邯郸银行"),
+    HHBANK(11L,"HHBANK", "黄河银行","黄河银行"),
+    HKB(11L,"HKB", "汉口银行","汉口银行"),
+    HKBEA(11L,"HKBEA", "东亚银行","东亚银行"),
+    HLDBANK(11L,"HLDBANK", "葫芦岛银行","葫芦岛银行"),
+    HNBANK(11L,"HNBANK", "海南银行","海南银行"),
+    HSB(11L,"HSB", "衡水银行","衡水银行"),
+    HSBANK(11L,"HSBANK", "徽商银行","徽商银行"),
+    HSBC(11L,"HSBC", "汇丰银行","汇丰银行"),
+    HSBK(11L,"HSBK", "恒生银行","恒生银行"),
+    HUARUIBANK(11L,"HUARUIBANK", "华瑞银行","华瑞银行"),
+    HUNB(11L,"HUNB", "湖南银行","湖南银行"),
+    HXB(11L,"HXB", "华夏银行","华夏"),
+    HZCB(11L,"HZCB", "杭州银行","杭州"),
+    hzccb(11L,"hzccb", "湖州银行","湖州银行"),
+    ICBC(11L,"ICBC", "中国工商银行","工商"),
+    JCCB(11L,"JCCB", "晋城银行","晋城银行"),
+    JDZBANK(11L,"JDZBANK", "景德镇银行","景德镇银行"),
+    JHBANK(11L,"JHBANK", "金华银行","金华银行"),
+    JJCCB(11L,"JJCCB", "九江银行","九江银行"),
+    JLBANK(11L,"JLBANK", "吉林银行","吉林银行"),
+    JSBC(11L,"JSBC", "江苏银行","江苏"),
+    JSHB(11L,"JSHB", "晋商银行","晋商银行"),
+    JXCCB(11L,"JXCCB", "嘉兴银行","嘉兴银行"),
+    JZB(11L,"JZB", "焦作银行","焦作银行"),
+    JZBA(11L,"JZBA", "晋中银行","晋中银行"),
+    LANGFB(11L,"LANGFB", "廊坊银行","廊坊银行"),
+    LJBANK(11L,"LJBANK", "龙江银行","龙江银行"),
+    LSBANK(11L,"LSBANK", "莱商银行","莱商银行"),
+    LSBC(11L,"LSBC", "临商银行","临商银行"),
+    LSBK(11L,"LSBK", "辽沈银行","辽沈银行"),
+    LXBANK(11L,"LXBANK", "临沂银行","临沂银行"),
+    LYBANK(11L,"LYBANK", "洛阳银行","洛阳银行"),
+    LYCB(11L,"LYCB", "辽阳银行","辽阳银行"),
+    LZBANK(11L,"LZBANK", "泸州银行","泸州银行"),
+    LZCCB(11L,"LZCCB", "柳州银行","柳州银行"),
+    LZYH(11L,"LZYH", "兰州银行","兰州银行"),
+    MASBANK(11L,"MASBANK", "马鞍山银行","马鞍山银行"),
+    MTBANK(11L,"MTBANK", "马鞍山银行","民泰银行"),
+    NBBANK(11L,"NBBANK", "宁波银行","宁波"),
+    NCB(11L,"NCB", "江西银行","江西银行"),
+    NJCB(11L,"NJCB", "南京银行","南京银行"),
+    NTBANK(11L,"NTBANK", "南通银行","南通银行"),
+    NXBANK(11L,"NXBANK", "宁夏银行","宁夏银行"),
+    PDSB(11L,"PDSB", "平顶山银行","平顶山银行"),
+    PERB(11L,"PERB", "蒙商银行","蒙商银行"),
+    PJBANK(11L,"PJBANK", "盘锦银行","盘锦银行"),
+    PLBANK(11L,"PLBANK", "平凉银行","平凉银行"),
+    PSBC(11L,"PSBC", "中国邮政储蓄银行","邮储银行"),
+    PSBCB(11L,"PSBCB", "邮政银行","邮政银行"),
+    PYBANK(11L,"PYBANK", "濮阳银行","濮阳银行"),
+    QDCCB(11L,"QDCCB", "青岛银行","青岛银行"),
+    QHBK(11L,"QHBK", "青海银行","青海银行"),
+    QHDBANK(11L,"QHDBANK", "秦皇岛银行","秦皇岛银行"),
+    QLBANK(11L,"QLBANK", "齐鲁银行","齐鲁银行"),
+    QSB(11L,"QSB", "齐商银行","齐商银行"),
+    QTBANK(11L,"QTBANK", "其他银行","其他银行"),
+    QZCCB(11L,"QZCCB", "泉州银行","泉州银行"),
+    RBOZ(11L,"RBOZ", "华润银行","华润银行"),
+    SBANK(11L,"SBANK", "微商银行","微商银行"),
+    SCB(11L,"SCB", "四川银行","四川银行"),
+    SCHK(11L,"SCHK", "渣打银行","渣打银行"),
+    SPDB(11L,"SPDB", "上海浦东发展银行","上海浦东发展银行"),
+    WHCCB(11L,"WHCCB", "威海银行","上海浦东发展银行"),
+    ;
+
+    private Long id;
+    private String code;
+    private String bankName;
+    private String bankShortName;
+
+    TyBankTypeEnum(Long id, String code, String bankName, String bankShortName) {
+        this.id = id;
+        this.code = code;
+        this.bankName = bankName;
+        this.bankShortName = bankShortName;
+    }
+
+    public static TyBankTypeEnum getType(Long id) {
+        for (TyBankTypeEnum enums : values()) {
+            if (enums.id.equals(id)) {
+                return enums;
+            }
+        }
+        return null;
+    }
+
+    public static TyBankTypeEnum getByName(String name) {
+        for (TyBankTypeEnum enums : values()) {
+            if (enums.bankName.equals(name) || enums.bankShortName.equals(name)) {
+                return enums;
+            }
+        }
+        return null;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public String getBankName() {
+        return bankName;
+    }
+}
