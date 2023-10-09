60dc726cb3bc6d223b4bec66aab459a275196d5c
orbit <orbit@dzpk.onmicrosoft.com>
2023-10-09 21:30:33 +0800

    增加提款存款订单信息处理工具类

---
+++ src/main/java/com/dx/facade/common/utils/WithdrawDepositInfoUtil.java
@@ -0,0 +1,18 @@
+package com.dx.facade.common.utils;
+
+/**
+ * @author orbit
+ * @datetime 2023-10-09 21:23
+ * @desc TODO
+ */
+public class WithdrawDepositInfoUtil {
+    public static String hideCardNo(String cardNo) {
+        //TODO 逻辑待完善
+        return cardNo;
+    }
+
+    public static String hideVirtualAddr(String virtualAddr) {
+        //TODO 逻辑待完善
+        return virtualAddr;
+    }
+}
