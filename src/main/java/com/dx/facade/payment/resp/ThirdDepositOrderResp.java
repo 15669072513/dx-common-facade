d7b9a137923aada2958d42095616161b7a8e061f
flop <flop@dzpk.onmicrosoft.com>
2023-10-14 15:27:35 +0800

    订单状态
    订单状态

---
+++ src/main/java/com/dx/facade/payment/resp/ThirdDepositOrderResp.java
@@ -0,0 +1,46 @@
+package com.dx.facade.payment.resp;
+
+import io.swagger.annotations.ApiModelProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ThirdDepositOrderResp {
+
+    @ApiModelProperty("查询结果，true查询成功，false三方返回查询失败")
+    private boolean result;
+
+    @ApiModelProperty("订单状态 ThirdDepositOrderStatus")
+    private Integer orderStatus;
+
+    @ApiModelProperty("订单金额")
+    private BigDecimal orderAmount;
+
+    @ApiModelProperty("实付金额")
+    private BigDecimal payAmount;
+
+    @ApiModelProperty(value = "虚拟币地址")
+    private String virtualAddress;
+
+    @ApiModelProperty(value = "虚拟币协议")
+    private Integer virtualProtocol;
+
+    @ApiModelProperty("返回信息")
+    private String message;
+
+    private ThirdDepositOrderResp(boolean result){
+        this.result = result;
+    }
+
+    public static ThirdDepositOrderResp fail(){
+        return new ThirdDepositOrderResp(false);
+    }
+
+}
