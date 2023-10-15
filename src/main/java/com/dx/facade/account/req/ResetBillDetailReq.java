02ce17ceb1016b862f5882aa95f7a79abbd18e7a
orbit <orbit@dzpk.onmicrosoft.com>
2023-10-15 15:26:34 +0800

    增加流水重置REQ

---
+++ src/main/java/com/dx/facade/account/req/ResetBillDetailReq.java
@@ -0,0 +1,35 @@
+package com.dx.facade.account.req;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+/**
+ * @author orbit
+ * @datetime 2023-10-15 11:46
+ * @desc 会员流水重置RPC入参
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ResetBillDetailReq implements Serializable {
+    /**
+     * 商户ID
+     */
+    private Long merchantId;
+    /**
+     * 会员ID
+     */
+    private Long memberId;
+    /**
+     * 流水重置操作人
+     */
+    private String operator;
+
+}
