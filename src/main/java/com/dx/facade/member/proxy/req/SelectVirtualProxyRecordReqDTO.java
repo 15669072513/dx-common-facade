7196dfd4eba969014a9035520537e8a282cd3e58
orbit <orbit@dzpk.onmicrosoft.com>
2023-10-16 15:27:31 +0800

    代理银行卡虚拟币账号使用记录参数修正

--- src/main/java/com/dx/facade/member/proxy/req/SelectVirtualProxyRecordReqDTO.java
+++ src/main/java/com/dx/facade/member/proxy/req/SelectVirtualProxyRecordReqDTO.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 /**
@@ -15,38 +16,38 @@ public class SelectVirtualProxyRecordReqDTO extends SelectPagingQueryReqDTO{
     @ApiModelProperty(value = "商户id", hidden = true)
     @JsonFormat(shape = JsonFormat.Shape.STRING)
     private Long merchantId;
-
+/*
     @ApiModelProperty(value = "代理id")
     @JsonFormat(shape = JsonFormat.Shape.STRING)
-    private Long userId;
+    private Long userId;*/
 
     @ApiModelProperty(value = "使用时间-开始日期")
-    private String beginDate;
+    private LocalDateTime beginDate;
 
     @ApiModelProperty(value = "使用时间-结束日期")
-    private String endDate;
+    private LocalDateTime endDate;
 
     @ApiModelProperty(value = "代理账号")
     private String userName;
 
-    @ApiModelProperty(value = "代理类型")
-    private List<String> accountType;
+    @ApiModelProperty(value = "代理类型：1-正式")
+    private Integer accountType;
 
     @ApiModelProperty(value = "虚拟币账户地址")
     private String virtualAddress;
 
-    @ApiModelProperty(value = "虚拟币种类")
+    @ApiModelProperty(value = "虚拟币种类，目前只有USDT,传入大写USDT即可")
     private String virtualKind;
 
-    @ApiModelProperty(value = "虚拟币协议")
-    private String virtualProtocol;
+    @ApiModelProperty(value = "虚拟币协议:1-omni协议，2-trc20，3-erc20")
+    private Integer virtualProtocol;
 
-    @ApiModelProperty("风控层级")
-    private List<String> windControlId;
+    @ApiModelProperty("取款账号风控层级名称")
+    private String accountWindControl;
 
-    @ApiModelProperty(value = "数据类型（1-银行卡 2-虚拟货币）",hidden = true)
-    private Integer dataType;
+    /*@ApiModelProperty(value = "数据类型（1-银行卡 2-虚拟货币）",hidden = true)
+    private Integer dataType;*/
 
-    @ApiModelProperty("排序方式(1)：desc-降序 asc-升序")
-    private String orderType;
+    /*@ApiModelProperty("排序方式(1)：desc-降序 asc-升序")
+    private String orderType;*/
 }
