5fc08dd5ced1b39e4c8c79424f3996b86949989a
moka <justgogoo@outlook.com>
2023-10-17 18:25:49 +0800

    bugfix

--- src/main/java/com/dx/facade/game/service/InsuranceInfoRpcService.java
+++ src/main/java/com/dx/facade/game/service/InsuranceInfoRpcService.java
@@ -4,8 +4,14 @@ import com.dx.entity.CommonResp;
 import com.dx.entity.PageResp;
 import com.dx.facade.game.req.InsuranceInfoListReq;
 import com.dx.facade.game.resp.InsuranceInfoResp;
+import com.dx.facade.texas.resp.InsureIsOursRecordResp;
+
+import java.util.List;
 
 public interface InsuranceInfoRpcService {
 
   CommonResp<PageResp<InsuranceInfoResp, ?>> getInsuranceInfoList(InsuranceInfoListReq req);
+
+  CommonResp<List<InsureIsOursRecordResp>> getInsuranceInfoByTableId(Long tableId);
+
 }
