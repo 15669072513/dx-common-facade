b2df8d788eed76934c506dc6137fe07021a3811e
ahead <mouse@mousedeMacBook-Pro.local>
2023-10-19 21:30:05 +0800

    fix：游戏牌局回顾查询

---
+++ src/main/java/com/dx/facade/es/service/GameOrderRecordReviewService.java
@@ -0,0 +1,30 @@
+package com.dx.facade.es.service;
+
+import com.dx.entity.CommonResp;
+import com.dx.entity.PageResp;
+import com.dx.facade.game.req.GameReviewReq;
+import com.dx.facade.game.resp.GameRecordDeatailPackResp;
+import com.dx.facade.game.resp.GameReviecardRoundResp;
+import com.dx.facade.game.resp.GameReviewcardResp;
+
+
+/**
+ * 游戏牌局回顾查询
+ * @author streak
+ */
+public interface GameOrderRecordReviewService {
+
+    /**
+     * 查询展示牌局回顾牌谱列表
+     * @param req
+     * @return
+     */
+    CommonResp<PageResp<GameReviewcardResp, GameReviecardRoundResp>> queryGameReviewList(GameReviewReq req);
+
+    /**
+     * 查看牌局回顾详细过程明细
+     * @param req
+     * @return
+     */
+    CommonResp<PageResp<GameRecordDeatailPackResp, Void>> getGameReviewDeatailList(GameReviewReq req);
+}
