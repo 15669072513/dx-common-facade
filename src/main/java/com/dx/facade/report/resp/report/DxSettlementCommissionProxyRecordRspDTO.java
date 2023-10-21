d40cd0e1fb2e2f163c1f3c3a57583932e6cc74ab
late <late@dzpk.onmicrosoft.com>
2023-10-21 16:53:58 +0800

    总代非总代增加代理风控字段

--- src/main/java/com/dx/facade/report/resp/report/DxSettlementCommissionProxyRecordRspDTO.java
+++ src/main/java/com/dx/facade/report/resp/report/DxSettlementCommissionProxyRecordRspDTO.java
@@ -78,6 +78,24 @@ public class DxSettlementCommissionProxyRecordRspDTO implements Serializable {
      */
     @ApiModelProperty("代理账号")
     private String proxyName;
+    /**
+     * 代理层级
+     */
+    @ApiModelProperty("代理层级")
+    private Long proxyLevel;
+
+    /**
+     * windControlName
+     */
+    @ApiModelProperty(value = "风控层级名称")
+    private String windControlName;
+
+
+    @ApiModelProperty("登陆锁定状态 0-未锁定 1-登陆锁定")
+    private Integer loginLockStatus;
+
+    @ApiModelProperty("充提锁定状态 0-未锁定 1-充提锁定")
+    private Integer paymentLockStatus;
 
     /**
      * 商户id
