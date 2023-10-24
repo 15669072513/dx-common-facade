54c672dd191cf3291bb4d11a3886712be329b254
buyin <buyin@dx.com>
2023-10-24 19:16:28 +0800

    结算优化

---
+++ src/main/java/com/dx/facade/texas/resp/HandStatusResp.java
@@ -0,0 +1,21 @@
+package com.dx.facade.texas.resp;
+public class HandStatusResp  {
+    private Integer status;
+    private String handCode;
+
+    public Integer getStatus() {
+        return status;
+    }
+
+    public void setStatus(Integer status) {
+        this.status = status;
+    }
+
+    public String getHandCode() {
+        return handCode;
+    }
+
+    public void setHandCode(String handCode) {
+        this.handCode = handCode;
+    }
+}
