034c0527deb34ca7dbb17db2e5aee7d68c879f51
bicycle <bicycle@dzpk.onmicrosoft.com>
2023-10-30 14:21:21 +0800

    feat：优化代码

--- src/main/java/com/dx/facade/member/resp/ProxyCommissionSettleCycleRespDTO.java
+++ src/main/java/com/dx/facade/member/resp/ProxyCommissionSettleCycleRespDTO.java
@@ -1,5 +1,7 @@
 package com.dx.facade.member.resp;
 
+import java.time.LocalDateTime;
+
 import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -15,4 +17,7 @@ public class ProxyCommissionSettleCycleRespDTO {
     @ApiModelProperty("佣金结算周期 1：半月结 2：月结")
     private Integer commissionSettleCycle;
 
+    public static void main(String[] args) {
+		System.out.println(LocalDateTime.now().getDayOfMonth());
+	}
 }
