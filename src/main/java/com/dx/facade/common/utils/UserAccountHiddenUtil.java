75a039ecf16ee953a77f963b65f6d80e3ecb0d4a
orbit <orbit@dzpk.onmicrosoft.com>
2023-11-04 00:04:32 +0800

    增加ip脱敏方法

--- src/main/java/com/dx/facade/common/utils/UserAccountHiddenUtil.java
+++ src/main/java/com/dx/facade/common/utils/UserAccountHiddenUtil.java
@@ -28,4 +28,18 @@ public class UserAccountHiddenUtil {
             }
             return start + star + end;
     }
+
+    public static String hideIp(String source){
+        if(StringUtils.isBlank(source) || source.length() < 4) {
+            return source;
+        }
+        String start = source.substring(0,2);
+        String end = source.substring(source.length()-2,source.length());
+        int starCount = source.length() - 4;
+        StringBuilder star = new StringBuilder();
+        for (int i = 0; i < starCount; i++){
+            star.append("*");
+        }
+        return start + star + end;
+    }
 }
