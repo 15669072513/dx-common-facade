03ae288ff0aa87fedef84d8e9544f0ef186123b3
buyin <buyin@dx.com>
2023-11-09 13:10:53 +0800

    结算rpc新增字段

--- src/main/java/com/dx/facade/texas/service/ISettlementRpcService.java
+++ src/main/java/com/dx/facade/texas/service/ISettlementRpcService.java
@@ -3,6 +3,6 @@ package com.dx.facade.texas.service;
 import com.dx.entity.CommonResp;
 import com.dx.facade.texas.dto.DxSettlementDto;
 
-public interface SettlementRpcService {
- CommonResp<Boolean> settlement(DxSettlementDto dxSettlementDto);
+public interface ISettlementRpcService {
+    CommonResp<Integer> settlement(DxSettlementDto dxSettlementDto);
 }
