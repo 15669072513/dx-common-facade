f84d20bea9adf4e703a664a18260e723d3a0defd
royal <justgogoo@outlook.com>
2023-11-11 00:47:49 +0800

    bugfix

--- src/main/java/com/dx/facade/game/service/UserTableAggRpcService.java
+++ src/main/java/com/dx/facade/game/service/UserTableAggRpcService.java
@@ -2,11 +2,11 @@ package com.dx.facade.game.service;
 
 import com.dx.entity.CommonResp;
 import com.dx.entity.PageResp;
+import com.dx.facade.game.dto.game.GameOrderRecordUserNetChipSumReqDto;
+import com.dx.facade.game.req.GameOrderRecordUserNetChipSumReq;
 import com.dx.facade.game.req.GameOrderUserTableSumReq;
-import com.dx.facade.game.req.RecordDetailReq;
 import com.dx.facade.game.req.UserTableReq;
 import com.dx.facade.game.resp.GameOrderUserTableSumResp;
-import com.dx.facade.game.resp.UserTableAggListResp;
 import com.dx.facade.game.resp.gamedetail.GameOrderRecordDetail;
 
 import java.util.List;
@@ -20,6 +20,13 @@ public interface UserTableAggRpcService {
      * @return
      */
     CommonResp<List<Long>> getUserTableListByUserId(UserTableReq userTableReq);
+
+    /**
+     * 盈亏总额
+     * @param req
+     * @return
+     */
+    CommonResp<GameOrderRecordUserNetChipSumReqDto> userNetAmountSummary(GameOrderRecordUserNetChipSumReq req);
 
     /**
      * 查询战绩列表
@@ -42,5 +49,6 @@ public interface UserTableAggRpcService {
      * @return
      */
     CommonResp<List<GameOrderRecordDetail.GameOrderRecord>> getGameOrderPlayerRecords(Long tableId);
+
 
 }
