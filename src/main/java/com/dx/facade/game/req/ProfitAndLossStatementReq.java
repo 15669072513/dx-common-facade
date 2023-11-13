5dd7968e3190ea581b35fd20b6d90756f7f77d55
royal <justgogoo@outlook.com>
2023-11-13 17:35:40 +0800

    bugfix

--- src/main/java/com/dx/facade/game/req/ProfitAndLossStatementReq.java
+++ src/main/java/com/dx/facade/game/req/ProfitAndLossStatementReq.java
@@ -18,12 +18,12 @@ public class ProfitAndLossStatementReq {
     private String id;
 
     @ApiModelProperty("转账时间开始")
-    @DateTimeFormat(pattern = "yyyy-MM-dd")
-    @JsonFormat(pattern = "yyyy-MM-dd", timezone = "GMT+8")
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
     private LocalDateTime profitStartTime;
 
     @ApiModelProperty("转账时间结束")
-    @DateTimeFormat(pattern = "yyyy-MM-dd")
-    @JsonFormat(pattern = "yyyy-MM-dd", timezone = "GMT+8")
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
     private LocalDateTime profitEndTime;
 }
