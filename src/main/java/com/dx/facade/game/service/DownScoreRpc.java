ab478755e648dc30b85b5411b99c54063a9efdfa
buyin <buyin@dx.com>
2023-11-17 22:00:08 +0800

    下分带出

---
+++ src/main/java/com/dx/facade/game/service/DownScoreRpc.java
@@ -0,0 +1,9 @@
+package com.dx.facade.game.service;
+
+import com.dx.entity.CommonResp;
+
+import java.math.BigDecimal;
+
+public interface DownScoreRpc {
+    CommonResp<Void> downScore(Long tableId, BigDecimal amount);
+}
