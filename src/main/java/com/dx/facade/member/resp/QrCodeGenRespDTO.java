98ba6d78d51b611a65f25be0fe345dc61fe6ec23
Middle <middle@gmail.com>
2023-11-17 23:27:11 +0800

    增加二维码

--- src/main/java/com/dx/facade/member/resp/QrCodeGenRespDTO.java
+++ src/main/java/com/dx/facade/member/resp/QrCodeGenRespDTO.java
@@ -1,12 +1,13 @@
 package com.dx.facade.member.resp;
 
-import com.dx.util.RandomUtil;
+import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 import java.io.Serializable;
 
 @Data
+@ApiModel("生成二维码")
 public class QrCodeGenRespDTO implements Serializable {
 
     @ApiModelProperty(value = "二维码内容：https://integrative-web-fat.ak12.cc/#/home?i_code=9340752&sc=R99Ua2Px")
--- src/main/java/com/dx/facade/member/resp/QrCodeParseRespDTO.java
+++ src/main/java/com/dx/facade/member/resp/QrCodeParseRespDTO.java
@@ -1,5 +1,6 @@
 package com.dx.facade.member.resp;
 
+import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.*;
 
@@ -7,6 +8,7 @@ import java.io.Serializable;
 
 @EqualsAndHashCode(callSuper = true)
 @Data
+@ApiModel("会员扫码成功")
 public class QrCodeParseRespDTO extends MemberVO implements Serializable {
 
     @ApiModelProperty(value="二维码类型：1-用户码，2-用户牌桌码，3-牌桌码")
