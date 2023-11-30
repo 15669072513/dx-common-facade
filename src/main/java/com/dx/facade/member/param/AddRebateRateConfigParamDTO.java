64af4ba80ce1903b24e3edbb093c3f4da6430f2a
bicycle <bicycle@dzpk.onmicrosoft.com>
2023-11-30 13:56:44 +0800

    feat:优化代码

--- src/main/java/com/dx/facade/member/param/AddRebateRateConfigParamDTO.java
+++ src/main/java/com/dx/facade/member/param/AddRebateRateConfigParamDTO.java
@@ -6,6 +6,7 @@ import java.time.LocalDateTime;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.NoArgsConstructor;
 
 /**
@@ -16,6 +17,7 @@ import lombok.NoArgsConstructor;
  * @author bicycle
  * @since 2023-7-22
  */
+@Data
 @NoArgsConstructor
 @AllArgsConstructor
 @ApiModel(value = "新增RebateRateDTO对象", description = "新增返点比例DTO")
