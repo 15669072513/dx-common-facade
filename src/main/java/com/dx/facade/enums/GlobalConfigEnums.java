ee01a62a6e0e979cfb704fa0073bab1c0ba6a048
duplicate <duplicate@dx.com>
2023-12-20 20:34:17 +0800

    全局配置相关常量，枚举，接口定义

--- src/main/java/com/dx/facade/enums/GlobalConfigEnums.java
+++ src/main/java/com/dx/facade/enums/GlobalConfigEnums.java
@@ -1,20 +1,22 @@
 package com.dx.facade.enums;
 
 public enum GlobalConfigEnums {
-    TEXAS_RAISE(1, 100, "德州倍数加注"),
-    TEXAS_POT_RAISE(2, 101, "德州底池加注"),
-    SHORT_DECK_RAISE(3, 200, "短牌倍数加注"),
-    SHORT_DECK_POT_RAISE(4, 201, "短牌底池加注"),
-    CHAT_BARRAGE(5, 300, "聊天弹幕"),
-    CHAT_PHRASES(6, 301, "聊天短语"),
-    CHAT_EMOJ(7, 302, "表情"),
-    CHAT_VOICE(8, 303, "聊天语音"),
-    VIEW_UNREVEALED_CARD(9, 304, "查看未发布公牌");
+    TEXAS_RAISE(1, 100, "倍数加注","德州扑克","德州倍数加注"),
+    TEXAS_POT_RAISE(2, 101, "底池加注","德州扑克","德州底池加注"),
+    SHORT_DECK_RAISE(3, 200, "倍数加注","短牌","短牌倍数加注"),
+    SHORT_DECK_POT_RAISE(4, 201, "底池加注","短牌","短牌底池加注"),
+    CHAT_BARRAGE(5, 300, "聊天弹幕","德州扑克/短牌","聊天弹幕"),
+    CHAT_PHRASES(6, 301, "聊天短语","德州扑克/短牌","聊天短语"),
+    CHAT_EMOJ(7, 302, "表情","德州扑克/短牌","表情"),
+    CHAT_VOICE(8, 303, "聊天语音","德州扑克/短牌","聊天语音"),
+    VIEW_UNREVEALED_CARD(9, 304, "查看未发布公牌","德州扑克/短牌","查看未发布公牌");
     private Integer bizType;
     private Integer dbType;
     private String desc;
+    private String bizTypeName;
+    private String gameName;
 
-    GlobalConfigEnums(Integer bizType, Integer dbType, String desc) {
+    GlobalConfigEnums(Integer bizType, Integer dbType, String bizTypeName,String gameName, String desc) {
         this.bizType = bizType;
         this.dbType = dbType;
         this.desc = desc;
@@ -31,6 +33,7 @@ public enum GlobalConfigEnums {
     public String getDesc() {
         return desc;
     }
+
 
 
     public static GlobalConfigEnums getByBizType(Integer bizType) {
