6091b7e4f881c7840d05c727ee7996e83f1068cd
duplicate <duplicate@dx.com>
2023-12-21 13:07:29 +0800

    全局配置相关常量，枚举，接口定义：修改类名

---
+++ src/main/java/com/dx/facade/texas/req/DxGlobalConfigToggleUpdateReq.java
@@ -0,0 +1,24 @@
+package com.dx.facade.texas.req;
+
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+import java.io.Serializable;
+
+
+/**
+ * 全局开关类配置修改req
+ */
+@Data
+public class DxGlobalConfigToggleUpdateReq implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @ApiModelProperty("配置主键")
+    private Long id;
+    @ApiModelProperty("开关值")
+    private Integer toggleVal;
+    @ApiModelProperty("操作人id")
+    private String opId;
+
+
+}
