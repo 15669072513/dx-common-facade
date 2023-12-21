eaefba9aa57629b8a1ddaf924b46c5691485d8e8
duplicate <duplicate@dx.com>
2023-12-21 16:06:48 +0800

    全局配置相关常量，枚举，接口定义：修改类名

---
+++ src/main/java/com/dx/facade/texas/req/DxGlobalConfigRaiseUpdateReq.java
@@ -0,0 +1,24 @@
+package com.dx.facade.texas.req;
+
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+import java.io.Serializable;
+
+
+/**
+ * 全局配置--加注配置req
+ */
+@Data
+public class DxGlobalConfigRaiseUpdateReq implements Serializable {
+    private static final long serialVersionUID = 1L;
+    @ApiModelProperty("主键")
+    private Long id;
+    @ApiModelProperty("加注配置信息")
+    private String config;
+    @ApiModelProperty("加注类型")
+    private Integer type;
+    @ApiModelProperty("操作人id")
+    private String opId;
+
+}
