a2fcd64ec3cfa3a954d0f33bf2f3b568a413b25c
jackpot <dyq100488@sina.com>
2023-12-21 17:43:26 +0800

    新增查询代理账单主显示接口

--- src/main/java/com/dx/facade/report/service/ProxyOverviewRPCService.java
+++ src/main/java/com/dx/facade/report/service/ProxyOverviewRPCService.java
@@ -3,10 +3,7 @@ package com.dx.facade.report.service;
 import com.dx.exception.BizException;
 import com.dx.facade.common.chart.Option;
 import com.dx.facade.report.param.proxyoverview.ProxyOverviewCompareDataReqDTO;
-import com.dx.facade.report.resp.proxyoverview.ProxyOverviewPrimaryDataRespDTO;
-import com.dx.facade.report.resp.proxyoverview.ProxyOverviewSubordinateMemberRespDTO;
-import com.dx.facade.report.resp.proxyoverview.ProxyOverviewSubordinateProxyRespDTO;
-import com.dx.facade.report.resp.proxyoverview.ProxyOverviewTotalCommissionRespDTO;
+import com.dx.facade.report.resp.proxyoverview.*;
 
 public interface ProxyOverviewRPCService {
     ProxyOverviewPrimaryDataRespDTO queryPrimaryData(Long var1, Long var2);
@@ -18,4 +15,6 @@ public interface ProxyOverviewRPCService {
     ProxyOverviewTotalCommissionRespDTO queryTotalCommission(Long var1, Long var2);
 
     ProxyOverviewSubordinateMemberRespDTO querySubordinateMember(Long var1, Long var2);
+
+    ProxyOverviewMainDataRespDTO queryMainData(Long proxyId, Integer startDate, Integer endDate);
 }
