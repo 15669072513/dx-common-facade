cc1c226454f2e97de980282f42138d0239852ced
duplicate <duplicate@dx.com>
2023-12-22 17:26:33 +0800

    全局配置相关，创建人，修改人字段修改

--- src/main/java/com/dx/facade/texas/req/DxGlobalConfigChatPhrasesListReq.java
+++ src/main/java/com/dx/facade/texas/req/DxGlobalConfigChatPhrasesListReq.java
@@ -1,6 +1,7 @@
 package com.dx.facade.texas.req;
 
 import com.dx.facade.common.PageRequest;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 import java.io.Serializable;
@@ -12,4 +13,6 @@ import java.io.Serializable;
 @Data
 public class DxGlobalConfigChatPhrasesListReq extends PageRequest implements Serializable {
     private static final long serialVersionUID = 1L;
+    @ApiModelProperty("短语状态")
+    private Integer state;
 }
