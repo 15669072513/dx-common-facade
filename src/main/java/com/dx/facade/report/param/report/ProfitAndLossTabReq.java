2e0a7c046ce16589de4df57a143e7da94c017f46
jackpot <dyq100488@sina.com>
2023-12-23 11:23:40 +0800

    修改个人中心盈亏数据，总盈亏数据列表，场馆返水有效投注列表接口

--- src/main/java/com/dx/facade/report/param/report/ProfitAndLossTabReq.java
+++ src/main/java/com/dx/facade/report/param/report/ProfitAndLossTabReq.java
@@ -13,33 +13,17 @@ import lombok.NoArgsConstructor;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-@ApiModel(value = "ProfitAndLossReq", description = "盈亏详情列表请求对象")
+@ApiModel(value = "ProfitAndLossTabReq", description = "盈亏详情列表Tab请求对象")
 public class ProfitAndLossTabReq {
 
     @ApiModelProperty(value = "本期账单开始日期", example = "20231213")
     private Integer periodStDate;
     @ApiModelProperty(value = "本期账单结束日期", example = "20231221")
     private Integer periodEnDate;
-    @ApiModelProperty(value = "数据类型:\n" +
-            "ty=体育\n" +
-            "zr=真人\n" +
-            "dj=电竞\n" +
-            "qp=棋牌\n" +
-            "cp=彩票\n" +
-            "DZ_CARD=德州")
+    @ApiModelProperty(value = "数据类型:ty=体育,zr=真人,dj=电竞,qp=棋牌,cp=彩票,DZ_CARD=德州")
     private String bwVenueType;
-    @ApiModelProperty(value = "数据类型:\n" +
-            "1=场馆盈亏\n" +
-            "2=返水盈利\n" +
-            "3=有效投注",
-            required = true, example = "1")
+    @ApiModelProperty(value = "数据类型: 1=场馆盈亏，2=返水盈利，3=有效投注",required = true, example = "1")
     private Integer profitLossTab;
     @ApiModelProperty(value = "会员id")
     private Long memberId;
-
-    private BwVenueTypeEnum bwVenueTypeEnum;
-
-    private ProfitAndLossTypeEnum profitAndLossTypeEnum;
-
-    private String profitAndLossTypeStr;
 }
