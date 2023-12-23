b520e5f474f53a658b0c1e44ce0726b102a8cde9
jackpot <dyq100488@sina.com>
2023-12-23 15:20:44 +0800

    修改总盈亏列表接口

--- src/main/java/com/dx/facade/report/service/ProfitAndLossRPCService.java
+++ src/main/java/com/dx/facade/report/service/ProfitAndLossRPCService.java
@@ -2,6 +2,7 @@ package com.dx.facade.report.service;
 
 import com.dx.facade.report.param.report.ProfitAndLossReq;
 import com.dx.facade.report.param.report.ProfitAndLossTabReq;
+import com.dx.facade.report.param.report.ProfitAndLossTotalReq;
 import com.dx.facade.report.resp.report.DwmProfitAndLossDetailRespDTO;
 
 import java.util.List;
@@ -9,7 +10,7 @@ import java.util.List;
 public interface ProfitAndLossRPCService {
     DwmProfitAndLossDetailRespDTO getProfitAndLoss(ProfitAndLossReq var1);
 
-    List<DwmProfitAndLossDetailRespDTO> getAllProfitAndLoss(ProfitAndLossReq var1);
+    List<DwmProfitAndLossDetailRespDTO> getAllProfitAndLoss(ProfitAndLossTotalReq var1);
 
     List<DwmProfitAndLossDetailRespDTO> getAllProfitAndLossTab(ProfitAndLossTabReq var1);
 
