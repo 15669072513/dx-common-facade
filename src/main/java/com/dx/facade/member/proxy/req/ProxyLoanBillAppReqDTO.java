ed5468bf5f08356a49a044839664984ac896db34
blaze <1021909013@qq.com>
2023-12-25 17:41:56 +0800

    feature: update ProxyLoanBillAppReqDTO

--- src/main/java/com/dx/facade/member/proxy/req/ProxyLoanBillAppReqDTO.java
+++ src/main/java/com/dx/facade/member/proxy/req/ProxyLoanBillAppReqDTO.java
@@ -20,4 +20,16 @@ public class ProxyLoanBillAppReqDTO {
 
     @ApiModelProperty(value = "代理id-如果当前用户是代理则填充该值")
     private Long proxyId;
+
+    @ApiModelProperty(value = "开始日期", example = "20231225")
+    private Integer startDate;
+
+    @ApiModelProperty(value = "结束日期", example = "20231226")
+    private Integer endDate;
+
+    @ApiModelProperty(value = "过滤标志", notes = "0:不过滤，1：应还账款 2：可提现额，默认是0")
+    private Integer filterFlag = 0;
+
+    @ApiModelProperty(value = "账单类型", notes = "0-未出账单,1-已出账单")
+    private Integer billType;
 }
