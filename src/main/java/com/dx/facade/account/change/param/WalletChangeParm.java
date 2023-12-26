8cc663f36640e11bfe9d52a2cb3ce411574b9f5d
dealer <dealer@dzpk.onmicrosoft.com>
2023-12-26 15:30:07 +0800

    细化参数校验条件

--- src/main/java/com/dx/facade/account/change/param/WalletChangeParm.java
+++ src/main/java/com/dx/facade/account/change/param/WalletChangeParm.java
@@ -1,5 +1,6 @@
 package com.dx.facade.account.change.param;
 
+import com.dx.facade.account.change.*;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
@@ -9,7 +10,6 @@ import lombok.experimental.SuperBuilder;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.util.Assert;
 
-import com.dx.facade.account.change.IWalletChange;
 import com.dx.facade.member.exception.ErrorCode;
 
 import java.math.BigDecimal;
@@ -20,22 +20,22 @@ import java.time.LocalDateTime;
 @SuperBuilder
 @NoArgsConstructor
 @AllArgsConstructor
-@ApiModel(value="账变基础对象", description="账变基础对象")
+@ApiModel(value = "账变基础对象", description = "账变基础对象")
 public abstract class WalletChangeParm implements IWalletChangeParm {
 
-    @ApiModelProperty(value="账变订单号，幂等性。传递订单号，确保一笔交易不会重复执行")
+    @ApiModelProperty(value = "账变订单号，幂等性。传递订单号，确保一笔交易不会重复执行")
     private String eventId;
 
-    @ApiModelProperty(value="账变事件发生时间")
+    @ApiModelProperty(value = "账变事件发生时间")
     private LocalDateTime eventTime;
 
-    @ApiModelProperty(value="账变总金额")
+    @ApiModelProperty(value = "账变总金额")
     private BigDecimal amount;
 
-    @ApiModelProperty(value="账变备注")
+    @ApiModelProperty(value = "账变备注")
     private String remark;
 
-    @ApiModelProperty(value="账变业务")
+    @ApiModelProperty(value = "账变业务")
     private IWalletChange walletChange;
 
     @ApiModelProperty("通用扩展字段A")
@@ -48,12 +48,17 @@ public abstract class WalletChangeParm implements IWalletChangeParm {
     private BigDecimal extensionC;
 
     @Override
-    public void check(){
+    public void check() {
         //公共参数简单校验
         Assert.hasText(eventId, ErrorCode.MISSING_PARAM_EXCEPTION.messageAfter("eventId"));
         Assert.notNull(eventTime, ErrorCode.MISSING_PARAM_EXCEPTION.messageAfter("eventTime"));
         Assert.notNull(amount, ErrorCode.MISSING_PARAM_EXCEPTION.messageAfter("amount"));
-        Assert.isTrue(amount.compareTo(BigDecimal.ZERO) > 0, ErrorCode.PARAM_EXCEPTION.messageAfter("amount 应大于0"));
+
+        //只有代理流水返点允许amount为负的情况
+        if (!(walletChange instanceof ProxyWalletChange && walletChange.changeType().code().equals(ProxyChangeType.v2_cash_rebate.code()))) {
+            Assert.isTrue(amount.compareTo(BigDecimal.ZERO) > 0, ErrorCode.PARAM_EXCEPTION.messageAfter("amount 应大于0"));
+        }
+
         Assert.hasText(remark, ErrorCode.MISSING_PARAM_EXCEPTION.messageAfter("remark"));
         Assert.notNull(walletChange, ErrorCode.MISSING_PARAM_EXCEPTION.messageAfter("walletChange"));
     }
