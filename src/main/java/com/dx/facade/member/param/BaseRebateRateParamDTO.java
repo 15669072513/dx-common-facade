2554d2472d6aa6fcd0a98934cbcd15cdbf82c155
bicycle <bicycle@dzpk.onmicrosoft.com>
2023-12-27 19:26:05 +0800

    feat:优化代码

--- src/main/java/com/dx/facade/member/param/BaseRebateRateParamDTO.java
+++ src/main/java/com/dx/facade/member/param/BaseRebateRateParamDTO.java
@@ -34,7 +34,6 @@ public class BaseRebateRateParamDTO implements Serializable {
     private Long merchantId;
     
     @ApiModelProperty("用户类型 0：会员 1：代理")
-    @NotNull(message = "用户类型不能为空")
     private Integer userType;
     
     @ApiModelProperty("用户Id")
