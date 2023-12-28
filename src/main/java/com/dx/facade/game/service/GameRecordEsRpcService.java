ce0b66ccd19a24bd0c59b9305721c8069ab2059e
royal <justgogoo@outlook.com>
2023-12-28 15:27:00 +0800

    优化代码

--- src/main/java/com/dx/facade/game/service/GameRecordEsRpcService.java
+++ src/main/java/com/dx/facade/game/service/GameRecordEsRpcService.java
@@ -3,7 +3,9 @@ package com.dx.facade.game.service;
 import com.dx.entity.CommonResp;
 import com.dx.entity.PageResp;
 import com.dx.facade.game.req.GameOrderRecordProxyReq;
+import com.dx.facade.game.req.GameOrderRecordReq;
 import com.dx.facade.game.resp.GameOrderRecordProxyResp;
+import com.dx.facade.game.resp.GameOrderRecordResp;
 
 public interface GameRecordEsRpcService {
 
@@ -13,4 +15,12 @@ public interface GameRecordEsRpcService {
      * @return
      */
     CommonResp<PageResp<GameOrderRecordProxyResp, ?>> getGameRecord(GameOrderRecordProxyReq req);
+
+    /**
+     * 查询注单列表
+     * @param req
+     * @return
+     */
+    CommonResp<PageResp<GameOrderRecordResp, ?>> queryGameOrderList(GameOrderRecordReq req);
+
 }
