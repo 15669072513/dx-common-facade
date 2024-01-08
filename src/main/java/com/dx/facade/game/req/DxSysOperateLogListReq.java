a352e7909a416a0ab4c9af7fcef4bbefaeb6ea79
royal <justgogoo@outlook.com>
2024-01-09 01:29:44 +0800

    操作记录开发

---
+++ src/main/java/com/dx/facade/game/req/DxSysOperateLogListReq.java
@@ -0,0 +1,43 @@
+package com.dx.facade.game.req;
+
+import com.dx.facade.report.param.base.BaseRequest;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@ApiModel("操作日志记录列表请求实体")
+@Data
+public class DxSysOperateLogListReq extends BaseRequest {
+
+    private static final long serialVersionUID = 1L;
+
+    @ApiModelProperty(value = "所属商户Id")
+    private Long merchantId;
+
+    @ApiModelProperty(value = "模块编码(操作栏目)")
+    private Integer moduleCode;
+
+    @ApiModelProperty(value = "操作页面编码(操作页面)")
+    private Integer operatePageCode;
+
+    @ApiModelProperty(value = "操作IP")
+    private String requestIp;
+
+    @ApiModelProperty(value = "操作人")
+    private String operateUserName;
+
+    @ApiModelProperty("操作开始时间")
+    @JsonSerialize(using = LocalDateTimeSerializer.class)
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
+    private LocalDateTime operateStartTime;
+
+    @ApiModelProperty("操作结束时间")
+    @JsonSerialize(using = LocalDateTimeSerializer.class)
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
+    private LocalDateTime operateEndTime;
+}
