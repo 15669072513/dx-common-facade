6152ff7e0b858bfea1b48b06077d4fd36540086e
rebuy <rebuy@dzpk.onmicrosoft.com>
2024-01-09 23:04:05 +0800

    feat(数据日报): 德信日报 联调修改

--- src/main/java/com/dx/facade/report/resp/report/DxReportDResp.java
+++ src/main/java/com/dx/facade/report/resp/report/DxReportDResp.java
@@ -2,9 +2,7 @@ package com.dx.facade.report.resp.report;
 
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 import java.io.Serializable;
 import java.math.BigDecimal;
@@ -17,10 +15,12 @@ import java.time.LocalDateTime;
 @Getter
 @EqualsAndHashCode(callSuper = false)
 @ApiModel(value="德信数据日报对象", description="德信日报")
+@AllArgsConstructor
+@NoArgsConstructor
 public class DxReportDResp implements Serializable {
 
     private static final long serialVersionUID = 1L;
-    @ApiModelProperty("类别：0-官方，1-代理")
+    @ApiModelProperty("类别：0-官方，1-代理,3-总结")
     private String dataType;
     @ApiModelProperty(value = "日期,比如20210531")
     private Integer reportDate;
@@ -76,4 +76,21 @@ public class DxReportDResp implements Serializable {
     @ApiModelProperty(value = "平台收入(万)")
     private BigDecimal platformIncomeAmount;
 
+    public DxReportDResp(BigDecimal creditQuota, BigDecimal newCreditQuota, BigDecimal withdrawAmount, BigDecimal depositAmount, BigDecimal loanAmount, BigDecimal newLoanAmount, BigDecimal shouldRepaymentAmount, BigDecimal shouldPaymentAmount, Integer newEffectiveNum, Integer activeNum, BigDecimal validBetAmount, BigDecimal netAmount, BigDecimal memberRebateAmount, BigDecimal proxyRebateAmount, BigDecimal platformIncomeAmount) {
+        this.creditQuota = creditQuota;
+        this.newCreditQuota = newCreditQuota;
+        this.withdrawAmount = withdrawAmount;
+        this.depositAmount = depositAmount;
+        this.loanAmount = loanAmount;
+        this.newLoanAmount = newLoanAmount;
+        this.shouldRepaymentAmount = shouldRepaymentAmount;
+        this.shouldPaymentAmount = shouldPaymentAmount;
+        this.newEffectiveNum = newEffectiveNum;
+        this.activeNum = activeNum;
+        this.validBetAmount = validBetAmount;
+        this.netAmount = netAmount;
+        this.memberRebateAmount = memberRebateAmount;
+        this.proxyRebateAmount = proxyRebateAmount;
+        this.platformIncomeAmount = platformIncomeAmount;
+    }
 }
