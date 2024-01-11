28e08373a164a97f6c2156d35a6f358a77ceef19
Maniac <maniac@dzpk.onmicrosoft.com>
2024-01-11 21:55:10 +0800

    feature:代理列表上级代理id字段

--- src/main/java/com/dx/facade/member/param/ProxySubProxyListEsDto.java
+++ src/main/java/com/dx/facade/member/param/ProxySubProxyListEsDto.java
@@ -26,99 +26,100 @@ import java.util.List;
 @AllArgsConstructor
 @NoArgsConstructor
 public class ProxySubProxyListEsDto {
-	
-	@ApiModelProperty("代理id")
-	@JsonFormat(shape = JsonFormat.Shape.STRING)
-	private Long id;
-	
-	@ApiModelProperty("代理账号")
-	private String userName;
-	
-	@ApiModelProperty("下级代理人数")
-	private Integer underProxyNum;
-	
-	@ApiModelProperty("直属下级代理人数")
-	private Integer directlyUnderProxyNum;
-	
-	@ApiModelProperty("下级会员人数")
-	private Integer underMemberNum;
-	
-	@ApiModelProperty("直属下级会员人数")
-	private Integer directlyUnderMemberNum;
-	
-	@ApiModelProperty(value = "累计首存人数")
-	private Integer totalFirstDepositNum;
 
-	@ApiModelProperty(value = "注册方式：0-手动 1-自动")
-	private Integer registerType;
-	
-	@ApiModelProperty("注册时间")
-	@JsonSerialize(using = LocalDateTimeSerializer.class)
-	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
-	private LocalDateTime createDt;
-	
-	@ApiModelProperty(value = "最后登录时间")
-	@JsonSerialize(using = LocalDateTimeSerializer.class)
-	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
-	private LocalDateTime lastLoginTime;
-	
-	@ApiModelProperty(value = "离线天数 近xx天未登录")
-	private Integer offLineDays;
-	
-	@ApiModelProperty(value = "代理层级路径(代理账号)")
-	private List<String> proxyPathUsername;
-	
-	@ApiModelProperty(value = "登录权限：0-关闭 1-开启")
-	private Integer loginLockStatus;
-	
-	@ApiModelProperty(value = "开局权限：0-关闭 1-开启")
-	private Integer openLockStatus;
-	
-	@ApiModelProperty(value = "管理俱乐部权限：0-关闭 1-开启")
-	private Integer manageClubLockStatus;
-	
-	@ApiModelProperty(value = "俱乐部数量")
-	private Integer clubCount;
-	
-	@ApiModelProperty(value = "俱乐部人数")
-	private Integer clubMemberCount;
-	
-    @ApiModelProperty("佣金钱包余额")
-    private BigDecimal commissionBalance;
+  @ApiModelProperty("代理id")
+  @JsonFormat(shape = JsonFormat.Shape.STRING)
+  private Long id;
+  @ApiModelProperty("上级代理id")
+  private Long parentProxyId;
+  @ApiModelProperty("代理账号")
+  private String userName;
 
-    @ApiModelProperty("额度钱包余额")
-    private BigDecimal quotaBalance;
-    
-    @ApiModelProperty(value = "现金余额")
-    private BigDecimal centerWalletBalance;
-    
-    @ApiModelProperty(value = "信用钱包余额")
-    private BigDecimal creditWalletBalance;
+  @ApiModelProperty("下级代理人数")
+  private Integer underProxyNum;
 
-    @ApiModelProperty(value = "信用额度")
-    private BigDecimal creditQuota;
+  @ApiModelProperty("直属下级代理人数")
+  private Integer directlyUnderProxyNum;
 
-    @ApiModelProperty(value = "应收账款")
-    private BigDecimal accountsReceivable;
+  @ApiModelProperty("下级会员人数")
+  private Integer underMemberNum;
 
-	@ApiModelProperty(value = "应还借款" )
-	private BigDecimal borrowAmount;
+  @ApiModelProperty("直属下级会员人数")
+  private Integer directlyUnderMemberNum;
 
-	@ApiModelProperty(value = "外放额度" )
-	private BigDecimal dispatchAmount;
+  @ApiModelProperty(value = "累计首存人数")
+  private Integer totalFirstDepositNum;
 
-	@ApiModelProperty(value = "授信总额" )
-	private BigDecimal teamCreditTotal;
-    
-    @ApiModelProperty("备注")
-    private String remark;
+  @ApiModelProperty(value = "注册方式：0-手动 1-自动")
+  private Integer registerType;
 
-	@ApiModelProperty(value="累计存款金额")
-	private BigDecimal totalDepositAmount;
+  @ApiModelProperty("注册时间")
+  @JsonSerialize(using = LocalDateTimeSerializer.class)
+  @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
+  private LocalDateTime createDt;
 
-	@ApiModelProperty(value="累计提款金额")
-	private BigDecimal totalWithdrawAmount;
+  @ApiModelProperty(value = "最后登录时间")
+  @JsonSerialize(using = LocalDateTimeSerializer.class)
+  @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
+  private LocalDateTime lastLoginTime;
 
-	@ApiModelProperty(value = "支付密码重置审核状态 0 未审核   1 审核中")
-	private Integer payPasswordAudit = 0;
+  @ApiModelProperty(value = "离线天数 近xx天未登录")
+  private Integer offLineDays;
+
+  @ApiModelProperty(value = "代理层级路径(代理账号)")
+  private List<String> proxyPathUsername;
+
+  @ApiModelProperty(value = "登录权限：0-关闭 1-开启")
+  private Integer loginLockStatus;
+
+  @ApiModelProperty(value = "开局权限：0-关闭 1-开启")
+  private Integer openLockStatus;
+
+  @ApiModelProperty(value = "管理俱乐部权限：0-关闭 1-开启")
+  private Integer manageClubLockStatus;
+
+  @ApiModelProperty(value = "俱乐部数量")
+  private Integer clubCount;
+
+  @ApiModelProperty(value = "俱乐部人数")
+  private Integer clubMemberCount;
+
+  @ApiModelProperty("佣金钱包余额")
+  private BigDecimal commissionBalance;
+
+  @ApiModelProperty("额度钱包余额")
+  private BigDecimal quotaBalance;
+
+  @ApiModelProperty(value = "现金余额")
+  private BigDecimal centerWalletBalance;
+
+  @ApiModelProperty(value = "信用钱包余额")
+  private BigDecimal creditWalletBalance;
+
+  @ApiModelProperty(value = "信用额度")
+  private BigDecimal creditQuota;
+
+  @ApiModelProperty(value = "应收账款")
+  private BigDecimal accountsReceivable;
+
+  @ApiModelProperty(value = "应还借款")
+  private BigDecimal borrowAmount;
+
+  @ApiModelProperty(value = "外放额度")
+  private BigDecimal dispatchAmount;
+
+  @ApiModelProperty(value = "授信总额")
+  private BigDecimal teamCreditTotal;
+
+  @ApiModelProperty("备注")
+  private String remark;
+
+  @ApiModelProperty(value = "累计存款金额")
+  private BigDecimal totalDepositAmount;
+
+  @ApiModelProperty(value = "累计提款金额")
+  private BigDecimal totalWithdrawAmount;
+
+  @ApiModelProperty(value = "支付密码重置审核状态 0 未审核   1 审核中")
+  private Integer payPasswordAudit = 0;
 }
