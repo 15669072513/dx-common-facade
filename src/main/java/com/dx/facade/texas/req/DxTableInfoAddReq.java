485b8cc6dfa9256ec5cbe72d7711602388a532a4
broadway <broadway@dzpk.onmicrosoft.com>
2024-01-13 14:10:34 +0800

    入桌牌桌密码开发

--- src/main/java/com/dx/facade/texas/req/DxTableInfoAddReq.java
+++ src/main/java/com/dx/facade/texas/req/DxTableInfoAddReq.java
@@ -193,10 +193,10 @@ public class DxTableInfoAddReq implements Serializable {
     @ApiModelProperty("前注（倍BB）,支持小数点1位")
     private BigDecimal anteLimit;
 
-    @ApiModelProperty("语音聊天功能 0-关 1-开 默认为0")
+    @ApiModelProperty("语音聊天功能 0-关闭 1-开启 2-关闭隐藏")
     private Integer voiceChatSwitch;
 
-    @ApiModelProperty("入桌密码功能 0-关 1-开 默认为0")
+    @ApiModelProperty("入桌密码功能 0-关闭 1-开启 2-关闭隐藏")
     private Integer tableEntryPasswordSwitch;
 
     @ApiModelProperty("入桌密码")
