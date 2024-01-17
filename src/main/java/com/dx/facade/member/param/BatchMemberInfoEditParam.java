908c69f0423c60aa4d847ff17627407c3ee74e37
tilt <tilt@dzpk.onmicrosoft.com>
2024-01-18 00:07:46 +0800

    会员代理批量修改帐号状态

--- src/main/java/com/dx/facade/member/param/BatchMemberInfoEditParam.java
+++ src/main/java/com/dx/facade/member/param/BatchMemberInfoEditParam.java
@@ -1,6 +1,7 @@
 package com.dx.facade.member.param;
 
 
+import com.dx.facade.constant.Constants;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
@@ -9,11 +10,14 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Pattern;
 import java.io.Serializable;
 import java.util.List;
 
 @Data
-@ApiModel(value = "com-bw-pojo-req-member-MemberInfoEditParam", description = "批量修改会员信息编辑 BatchMemberInfoEditParam")
+@ApiModel(value = "批量修改会员/代理信息编辑", description = "批量修改会员/代理信息编辑")
 @AllArgsConstructor
 @Builder
 @NoArgsConstructor
@@ -22,22 +26,11 @@ public class BatchMemberInfoEditParam implements Serializable {
     @ApiModelProperty(value = "商户id")
     private Long merchantId;
 
-    @ApiModelProperty(value = "会员ID")
-    @JsonFormat(shape = JsonFormat.Shape.STRING)
-    private List<Long> userIdList;
+    @ApiModelProperty(value = "会员账号",required = true,example = "abc123")
+    @NotEmpty(message = "账号不能为空")
+    @Pattern(regexp = Constants.REG_4_11, message = Constants.USER_NAME_ERROR_MSG)
+    private List<String> userNameList;
 
-
-    @ApiModelProperty(value = "商户编码")
-    private String merchantCode;
-
-    @ApiModelProperty(value = "商户名称")
-    private String merchantName;
-
-    @ApiModelProperty(value = "会员账号")
-    private String userName;
-
-    @ApiModelProperty(value = "申请人")
-    private String applyName;
 
     @ApiModelProperty(value = "账号状态 修改后内容")
     private MemberInfoRpcEditAccountStatus accountStatusAfter;
