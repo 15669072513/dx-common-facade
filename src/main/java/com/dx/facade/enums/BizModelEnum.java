55e8445b3ed7e8bb2e8ffaa72e3a8f15fcf1c492
dealer <dealer@dzpk.onmicrosoft.com>
2024-01-18 17:29:04 +0800

    添加业务模式判断通用方法

--- src/main/java/com/dx/facade/enums/BizModelEnum.java
+++ src/main/java/com/dx/facade/enums/BizModelEnum.java
@@ -1,5 +1,8 @@
 package com.dx.facade.enums;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * 业务模式：0-现金+信用业务，1-现金业务，2-信用业务
  * @author orbit
@@ -23,5 +26,28 @@ public enum BizModelEnum {
 
 	public String desc(Integer code) {
 		return this.desc;
+	}
+
+	private static Map<Integer, BizModelEnum> code2Enum = new HashMap<>();
+	static {
+		for (BizModelEnum modelEnum : BizModelEnum.values()) {
+			code2Enum.put(modelEnum.code, modelEnum);
+		}
+	}
+
+	public static BizModelEnum getByCode(Integer code){
+		return code2Enum.get(code);
+	}
+
+	public boolean isAllMode(Integer mode) {
+		return mode != null && mode.intValue() == BizModelEnum.MIX.code();
+	}
+
+	public boolean isCash(Integer mode) {
+		return mode != null && mode.intValue() == BizModelEnum.CASH.code();
+	}
+
+	public boolean isCredit(Integer mode) {
+		return mode != null && mode.intValue() == BizModelEnum.CREDIT.code();
 	}
 }
