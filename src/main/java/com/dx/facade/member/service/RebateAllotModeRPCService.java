42e455fcee79a408a7dbab897a3534dda4373b96
bicycle <bicycle@dzpk.onmicrosoft.com>
2024-01-20 00:35:31 +0800

    feat：新增返点比率模板等级更新接口

--- src/main/java/com/dx/facade/member/service/RebateAllotModeRPCService.java
+++ src/main/java/com/dx/facade/member/service/RebateAllotModeRPCService.java
@@ -3,6 +3,7 @@ package com.dx.facade.member.service;
 import com.dx.entity.CommonResp;
 import com.dx.exception.BizException;
 import com.dx.facade.member.param.UpdateRebateAllotModeParamDTO;
+import com.dx.facade.member.param.UpdateRebateRateTemplateLevelParamDTO;
 /**
  * 返点分配模式RPC服务
  * 
@@ -17,5 +18,20 @@ public interface RebateAllotModeRPCService {
      * @param request
      */
 	CommonResp<Boolean> updateRebateAllotMode(UpdateRebateAllotModeParamDTO request) throws BizException;
+	
+    /**
+     * 代理返点比率模板等级更新
+     * 
+     * @param request
+     */
+	CommonResp<Boolean> updateProxyRebateRateTemplateLevel(UpdateRebateRateTemplateLevelParamDTO request) throws BizException;
+	/**
+	 * 会员返点比率模板等级更新
+	 * 
+	 * @param request
+	 * @return
+	 * @throws BizException
+	 */
+	CommonResp<Boolean> updateMemberRebateRateTemplateLevel(UpdateRebateRateTemplateLevelParamDTO request) throws BizException;
 	
 }
