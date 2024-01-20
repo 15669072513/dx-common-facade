ada5b04419b1a51c7b44cac14e2ea7a23d5bb7be
bicycle <bicycle@dzpk.onmicrosoft.com>
2024-01-20 21:46:24 +0800

    feat：添加非空校验

--- src/main/java/com/dx/facade/member/param/UpdateRebateAllotModeParamDTO.java
+++ src/main/java/com/dx/facade/member/param/UpdateRebateAllotModeParamDTO.java
@@ -2,6 +2,8 @@ package com.dx.facade.member.param;
 
 import java.io.Serializable;
 
+import javax.validation.constraints.NotNull;
+
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
@@ -25,18 +27,23 @@ public class UpdateRebateAllotModeParamDTO implements Serializable {
     private static final long serialVersionUID = 1L;
     
     @ApiModelProperty("用户Id")
+    @NotNull(message = "用户ID 不能为空")
     private Long userId;
     
     @ApiModelProperty("用户类型 0：会员  1：代理")
+    @NotNull(message = "用户类型 不能为空")
     private Integer userType;
     
     @ApiModelProperty("总代Id")
+    @NotNull(message = "总代ID 不能为空")
     private Long topProxyId;
     
     @ApiModelProperty("返点分配模式 1：自由模式 2:模板模式")
+    @NotNull(message = "分配模式 不能为空")
     private Integer rebateAllotMode;
     
     @ApiModelProperty("模板Id")
+    @NotNull(message = "模板ID 不能为空")
     private Long templateId;
     
     @ApiModelProperty("比率等级")
