c830ad7c2f8c38e6f4d41ae192de303075d579ca
jackpot <jackpot@dzpk.onmicrosoft.com>
2024-01-21 19:43:29 +0800

    修改清除上月结余接口异常的问题
    Signed-off-by: jackpot <jackpot@dzpk.onmicrosoft.com>

--- src/main/java/com/dx/facade/report/service/ProxyLastMonthBalanceRPCService.java
+++ src/main/java/com/dx/facade/report/service/ProxyLastMonthBalanceRPCService.java
@@ -2,10 +2,11 @@ package com.dx.facade.report.service;
 
 import com.dx.entity.CommonResp;
 import com.dx.facade.report.param.report.DwmProxyLastMonthBalanceReq;
+import com.dx.facade.report.param.report.DwmProxyLastMonthOperationReq;
 import com.dx.facade.report.resp.report.DwmProxyLastMonthBalanceResqDTO;
 
 public interface ProxyLastMonthBalanceRPCService {
 
     CommonResp<DwmProxyLastMonthBalanceResqDTO> queryProxyLastMonthBalance (DwmProxyLastMonthBalanceReq var1);
-    CommonResp updateProxyLastMonthBalance (DwmProxyLastMonthBalanceReq var1);
+    CommonResp updateProxyLastMonthBalance (DwmProxyLastMonthOperationReq var1);
 }
