d8658454162d8ba12d93807c25e7b469b57022ce
streak <streak@dzpk.onmicrosoft.com>
2024-01-24 16:51:33 +0800

    订单用户天维度统计Map

--- src/main/java/com/dx/facade/game/resp/GameOrderAggSumResp.java
+++ src/main/java/com/dx/facade/game/resp/GameOrderAggSumResp.java
@@ -3,6 +3,7 @@ package com.dx.facade.game.resp;
 import io.swagger.annotations.ApiModelProperty;
 import java.math.BigDecimal;
 import java.util.List;
+import java.util.Map;
 import lombok.Data;
 
 /**
@@ -20,8 +21,8 @@ public class GameOrderAggSumResp {
   @ApiModelProperty(value = "总计实付金额")
   private BigDecimal totalPayAmount=BigDecimal.ZERO;
 
-  @ApiModelProperty(value = "订单用户天维度统计Dto")
-  private List<GameOrderAggDto> gameOrderAggList;
+  @ApiModelProperty(value = "订单用户天维度统计Map")
+  private Map<String,GameOrderAggDto> gameOrderAgMap;
 
 
 }
