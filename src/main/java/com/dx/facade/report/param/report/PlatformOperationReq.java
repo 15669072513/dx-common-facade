c078e48a2df90a4c0f458e7eaccd5bc0ef45a64b
jackpot <jackpot@dzpk.onmicrosoft.com>
2024-02-02 13:50:00 +0800

    定义平台运营状况概述接口参数及返回
    Signed-off-by: jackpot <jackpot@dzpk.onmicrosoft.com>

---
+++ src/main/java/com/dx/facade/report/param/report/PlatformOperationReq.java
@@ -0,0 +1,27 @@
+package com.dx.facade.report.param.report;
+
+import com.dx.facade.report.param.base.LocalDatePageRequest;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@ApiModel(value = "PlatformOperationReq", description = "平台运营状况概述请求对象", parent = LocalDatePageRequest.class)
+public class PlatformOperationReq extends LocalDatePageRequest {
+
+    @ApiModelProperty(value = "排序类型")
+    private String orderType;
+
+    @ApiModelProperty(value = "排序字段")
+    private String orderKey;
+
+    @ApiModelProperty(value = "查询范围 0:全量数据 1:统计日期", example = "1")
+    private String gameCode;
+
+}
