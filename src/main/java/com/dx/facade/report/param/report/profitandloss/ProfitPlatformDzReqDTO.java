4145a51c8c598d26233b99b0a88e1b134693269f
adu <admin@1222>
2024-02-15 03:28:44 +0800

    运营概况表优化更新需求开发

--- src/main/java/com/dx/facade/report/param/report/profitandloss/ProfitPlatformDzReqDTO.java
+++ src/main/java/com/dx/facade/report/param/report/profitandloss/ProfitPlatformDzReqDTO.java
@@ -6,12 +6,10 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.hibernate.validator.constraints.Range;
-import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
 import java.io.Serializable;
-import java.time.LocalDate;
 
 /**
  * 运营状况-德州数据 请求
@@ -31,19 +29,17 @@ public class ProfitPlatformDzReqDTO implements Serializable {
     @ApiModelProperty(value = "当前页,默认第1页", required = true, example = "1")
     @NotNull(message = "分页参数不能为空")
     @Min(value = 1, message = "当前页最小值不能小于1")
-    protected Long pageNum = 1L;
+    private Long pageNum = 1L;
 
     @ApiModelProperty(value = "每页条数,默认20条", required = true, example = "10")
     @NotNull(message = "分页参数不能为空")
     @Range(min = 1, max = 500, message = "每页条数必须 ≥ 1 并且 ≤ 500")
-    protected Long pageSize = 20L;
+    private Long pageSize = 20L;
 
-    @ApiModelProperty(value = "开始时间", required = true, example = "2021-06-17")
-    @DateTimeFormat(pattern = "yyyy-MM-dd")
-    protected LocalDate startTime;
+    @ApiModelProperty(value = "开始日期", required = true, example = "20210617")
+    protected Integer startDate;
 
-    @ApiModelProperty(value = "结束时间", required = true, example = "2021-06-17")
-    @DateTimeFormat(pattern = "yyyy-MM-dd")
-    protected LocalDate endTime;
+    @ApiModelProperty(value = "结束时间", required = true, example = "20210617")
+    private Integer endDate;
 
 }
