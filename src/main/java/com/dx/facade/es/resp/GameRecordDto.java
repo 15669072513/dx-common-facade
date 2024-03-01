045b7a13a6cf6b2bfa3a8e284278f3565d0d80fe
probe <probe@dzpk.onmicrosoft.com>
2024-03-01 14:52:20 +0800

    GameRecordDto更新(from roll)

--- src/main/java/com/dx/facade/es/resp/GameRecordDto.java
+++ src/main/java/com/dx/facade/es/resp/GameRecordDto.java
@@ -116,7 +116,10 @@ public class GameRecordDto {
     @ApiModelProperty("场馆名称(如ob真人,ag捕鱼)")
     private String venueName;
 
-    @ApiModelProperty("注单子类型:枚举GameRecordTypeEnum")
+    /**
+     * 注单类型 1 俱乐部游戏 2保险
+     */
+    @ApiModelProperty("注单类型:1俱乐部游戏;2保险")
     private Integer betType;
     @ApiModelProperty("注单类型:枚举GameRecordTypeEnum")
     private Integer parentBetType;
